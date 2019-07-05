76260a4d202ecc6e4456a40be5541809c750211b
shazmaan <shazmaan@buffalo.edu>
2019-07-06 01:24:10 +0400

    Finished Problem3.

---
+++ Problem3.cpp
@@ -0,0 +1,37 @@
+//
+// Created by shazmaan on 7/6/2019.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution{
+public:
+    int prob(vector<int>& nums){
+        return findPeek(nums,0,nums.size()-1);
+    }
+private:
+    int findPeek(vector<int>& nums,int low, int high){
+        int mid;
+        while(low!=high){
+            mid = (low+high)/2;
+            if((mid+1)<nums.size() && nums[mid+1]>nums[mid]){
+                low = mid + 1;
+            }else if((mid-1)>=0 && nums[mid-1]>nums[mid]){
+                high = mid - 1;
+            }else{
+                return mid;
+            }
+        }
+        return low;
+    }
+};
+
+int main(){
+    vector<int> vec{1,2,3,1};
+    Solution s; cout<<s.prob(vec);
+    return 0;
+}
