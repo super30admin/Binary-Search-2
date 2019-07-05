215594d1991ef8f9360e876ac372092128cf5fb9
shazmaan <shazmaan@buffalo.edu>
2019-07-05 16:05:02 +0400

    Problem1 iterative+recursive solution.

--- Problem1.cpp
+++ Problem1.cpp
@@ -11,7 +11,7 @@ using namespace std;
 class Solution{
 public:
     void Problem1(vector<int>& arr, int target){
-       int start = binarySearch(arr,0,arr.size()-1,target);
+       int start = IterativeBinarySearch(arr,0,arr.size()-1,target);
        int end = start;
        if(start!=(-1)){
            for(int x = start+1; x<arr.size();x++){
@@ -49,10 +49,33 @@ private:
         }
         binarySearch(arr,low,high,target);
     }
+    int IterativeBinarySearch(vector<int>& arr, int l, int h, int target){
+        int low = l; int high = h; int mid;
+        while(low<=high){
+            mid = low+(high-low)/2;
+            if(arr[mid]==target){
+                if((mid-1)>=0){
+                    if(arr[mid-1]==target){
+                        high = mid - 1;
+                    }else{
+                        return mid;
+                    }
+                }else{
+                    return mid;
+                }
+            }else if(arr[mid]>target){
+                high = mid-1;
+            }else if(arr[mid]<target){
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+
 };
 
 int main(){
     vector<int> vec{8,8,8,8,8,8};
-    Solution s; s.Problem1(vec,9);
+    Solution s; s.Problem1(vec,8);
     return 0;
 }
