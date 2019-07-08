d38bd07be258c837fe61782526db28144e898d25
GAURAV JOSHI <gsj38@msstate.edu>
2019-07-08 01:14:19 -0500

    Completed exercise for Day4

--- problem2.cpp
+++ problem2.cpp
@@ -7,9 +7,20 @@ using namespace std;
 class Solution{
     public:
 
-        int BinarySearch()
-        {
-            return 1;
-        }
+    int findMin(vector<int>& arr) {
         
+        int start = 0;
+        int end = arr.size() - 1;
+        int middle;
+
+        while(start < end)
+        {   
+            middle = start + (end - start)/2;
+            if(arr[start] < arr[end]) return arr[start];
+            if(arr[middle] < arr[end]) end = middle;
+            else start = middle + 1;
+        }
+        return arr[start];
+
+    }
 };
