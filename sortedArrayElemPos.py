0268f1b01b3b4d8c893b559410e24e9f8c36f46d
Prajakta Ray <pray2202@gmail.com>
2019-10-09 23:55:52 -0500

    Completed Prob1

--- sortedArrayElemPos.py
+++ sortedArrayElemPos.py
@@ -1,39 +1,60 @@
-# Couldn't find the approach. Not clear on how to store& update first and last index and iterate over the array
+#Submitted successfully to Leetcode
+#Time com : O(logn)
+#Space Com : O(1)
 class Solution:
-    def search(self,nums, target):
-        if not nums:
-            return -1
-        left = 0
-        right = len(nums)-1
-        first , last= -1,-1
-
-        while left<len(nums):
-            mid = (left+right)//2
-            print(mid, left,right)
-
-            if target < nums[mid]:
-                print("c")
-                right = mid-1
-            elif target > nums[mid]:
-                print("d")
-                left = mid+1
-            elif target == nums[mid]:
-                if first == -1 and last == -1:
-                    first, last = mid,mid
-                    if target < nums[right]:
-                        right = right-1
-                    elif target > nums[left]:
-                        left = left + 1
-                if first > left:
-                    print("e")
-                    first = left
-                right= mid-1
-                if last < right:
-                    print("f")
-                    last = right
-                left = mid+1
-
+    def searchRange(self, nums, target):
+        first,last = self.searchFirst(nums,target),self.searchLast(nums,target)
         return first,last
+
+
+    def searchFirst(self,nums,target):
+        first = -1
+        low = 0
+        high = len(nums)-1
+
+        while low<=high:
+            mid = (low+high)//2
+            # If target == array mid elem
+            if nums[mid] == target:
+                #if array has one elem or prev elem of the mid is less than the target
+                if low == mid or nums[mid-1] < target:
+                    first = mid
+                    break
+                #if prev elem of mid is equal to target
+                else:
+                    high = mid -1
+            #If target is > than mid then shit low to mid+1 elem
+            elif nums[mid] < target:
+                low = mid+1
+            #If target is < than mid then shit high to mid-1 elem
+            else:
+                high = mid-1
+        return first
+
+    def searchLast(self,nums,target):
+        last = -1
+        low = 0
+        high = len(nums)-1
+
+        while low<=high:
+            mid = (low+high)//2
+            # If target == array mid elem
+            if nums[mid] == target:
+                #if array has one elem or next elem of the mid is more than the target
+                if high == mid or nums[mid+1] > target:
+                    last = mid
+                    break
+                #if next elem of mid is equal to target
+                else:
+                    low = mid+1
+            #If target is > than mid then shit low to mid+1 elem
+            elif nums[mid] < target:
+                low = mid+1
+            #If target is < than mid then shit high to mid-1 elem
+            else:
+                high = mid-1
+        return last
+
 s = Solution()
-print(s.search([5,7,8,9,9,10],9))
-print(s.search([5,7,7,8,8,10],4))
+print(s.searchRange([5,7,8,9,9,10],9))
+print(s.searchRange([5,7,7,8,8,10],8))
