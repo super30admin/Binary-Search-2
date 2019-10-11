44f49f779f0086977881fd969a6c14d6e6826500
Prajakta Ray <pray2202@gmail.com>
2019-10-11 01:53:00 -0500

    Completed Prob2 &  Prob3

--- minRotatedArray.py
+++ minRotatedArray.py
@@ -1,36 +1,26 @@
 
-# Cannot think of ALL test cases to check the correctness of the algo
 # Time Com : O(lgn) Space Com : O(1)
-# No link for Leetcode so couldn't run it there
+# Ran successfully on leetcode
 
 class Solutions:
     def searchMin(self,nums):
-        low = 0
-        high = len(nums) -1
-        min = -1
-        while low <= high:
-            mid = (low+high)//2
-            if low == high:
-                return min
-            #updating value of min
-            if min == -1 or nums[mid] < min:
-                min = nums[mid]
-            #case 1 : when low > mid ...min definitely lies on the left hand-side
-            if(nums[low] > nums[mid]):
-                high = mid - 1
-            #case 2 : when low < mid ...leftmost elem maybe the min or min may lie on the right hand-side
-            elif(nums[low] < nums[mid]):
-                min = nums[low]
-                low = mid + 1
-            #case 3 : when  mid > high ...min definitely lies on the right hand-side
-            elif(nums[mid] > nums[high]):
-                low = mid + 1
-            #case 4 : when mid < high ...rightmost elem maybe the min or min may lie on the left hand-side
-            else:
-                min = nums[high]
-                high = mid -1
-        return -1
+            low = 0
+            high = len(nums) -1
+            min = -1
+            while low <= high:
+                mid = (low+high)//2
+                #when its  a simple sortd array
+                if nums[low] < nums[high]:
+                    return nums[low]
+                if (mid == high or nums[mid] < nums[mid+1]) and (mid == low or nums[mid] < nums[mid-1]):
+                    return nums[mid]
+                #left array sorted so min will be on the right side
+                elif nums[low] <= nums[mid]:
+                    low = mid+1
+                else:
+                    high = mid-1
+            return -1
 obj = Solutions()
-print(obj.searchMin([3,4,5,1,2,3]))
+print(obj.searchMin([3,4,5,0,1,2]))
 print(obj.searchMin([4,5,6,7,0,1,2]))
 print(obj.searchMin([8,1,2,3,4,5,6]))
--- peakElem.py
+++ peakElem.py
@@ -1,21 +1,23 @@
-# Couldn't find the solution for when there is no peak Eg : [8,7,5,2,5,7,8]
-# Didn't understand meaning of nums[-1] = nums[n] = -∞
-# Curious about the case where nums[i] == nums[i+1]
 # Time com : O(logn) Space Com : O(1)
-# No leetcode link to run the code
+# Successful on leetcode
 
 
 class Solution:
     def findPeak(self,nums):
         low = 0
         high = len(nums)-1
-        while low < high:
+        while low <= high:
             mid = (low+high)//2
-
-
-            if nums[mid-1] < nums[mid] and nums[mid] > nums[mid+1]:
+            #if mid is the peak then return mid. mid==low and mid==high are the edge cases we have to
+            #check else it will give array index out of bound
+            if (mid == low or nums[mid-1] < nums[mid]) and (mid == high or nums[mid] > nums[mid+1]):
                 return mid
             elif nums[mid+1] > nums[mid]:
                 low = mid+1
-            else nums[mid-1] > nums[mid]:
+            else:
                 high = mid-1
+        return -1
+
+obj = Solution()
+print(obj.findPeak([1,2,1,3,5,6,4]))
+print(obj.findPeak([1,2]))
