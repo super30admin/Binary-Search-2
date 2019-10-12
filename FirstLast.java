fa263deead8064e50d6da6b8163093f8d9a76c07
Shridhar Saurin Shah <42818678+Shridhar-2205@users.noreply.github.com>
2019-10-11 18:33:48 -0700

    Create FirstLast.java

---
+++ FirstLast.java
@@ -0,0 +1,101 @@
+
+class Solution {
+    
+    
+    private int first(int nums[],int target){
+        
+        int low = 0;
+        int high = nums.length-1;
+        
+        while (low<=high){
+            
+            int mid = low + (high-low)/2;
+            
+            
+            //case 1
+            
+            if(nums[mid] == target){
+                
+                if (mid == low || nums[mid-1] < nums[mid])
+                    return mid;
+                else
+                    high = mid -1;
+            }
+            
+            
+            //case 2
+            
+            else if(nums[mid]>target){
+                high = mid -1;
+            }
+            
+            //case 3
+            
+            else 
+                low = mid + 1;
+        }
+        
+        return -1;
+        
+        
+    }
+    
+     private int second(int nums[],int target){
+         
+        int low = 0;
+        int high = nums.length-1;
+         
+         
+        
+        while (low<=high){
+            
+            int mid = low + (high-low)/2;
+            
+            //case 1
+            
+            if(nums[mid] == target){
+                
+                if (mid == high || nums[mid+1] > nums[mid])
+                    return mid;
+                else
+                    low = mid + 1;
+            }
+            
+            
+            //case 2
+            
+            else if(nums[mid]>target){
+                high = mid -1;
+            }
+            
+            //case 3
+            
+            else 
+                low = mid + 1;
+        }
+        
+        return -1;
+        
+        
+    }
+    
+    
+    
+    public int[] searchRange(int[] nums, int target) {
+        
+        int first_index = first(nums,target);
+        int second_index = second(nums,target);
+        
+        return new int[] {first_index,second_index};
+        
+        
+        
+        
+        
+    }
+}
+
+//Time Complexity: O(Logn)
+//Space Complexity: O(1)
+
+
