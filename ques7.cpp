4ee27321415b035f4bc128ed1125222f81882f5c
ShreyasP7495 <56066380+ShreyasP7495@users.noreply.github.com>
2019-12-26 19:12:19 -0500

    Finding first and last position of the given element in Sorted Array

---
+++ ques7.cpp
@@ -0,0 +1,80 @@
+Time Complexity- O(log n)
+Space Complexity-O(n)
+Did the code execute on Leetcode- Yes
+
+#include <iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+private:
+    int lowerbound(vector<int>&nums,int target)
+    {
+        int low=0,high=nums.size()-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(nums[mid]>=target)
+            {
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        if(low==nums.size()|| nums[low]!=target)
+        {
+            return -1;
+        }
+        return low;
+    }
+    
+    int upperbound(vector<int>&nums,int target)
+    {
+        int low=0,high=nums.size()-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(nums[mid]<=target)
+            {
+                low=mid+1;
+            }
+            else
+            {
+                high=mid-1;
+            }
+        }
+        if(high==-1 || nums[high]!=target)
+        {
+            return -1;
+        }
+        return high;
+    }
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int>m={-1,-1};
+        int l=0,h=nums.size()-1,k=0,q=0;
+        m[0]=lowerbound(nums,target);
+        m[1]=upperbound(nums,target);
+        return m;
+    }
+};
+
+int main() {
+    vector<int>nums={5,7,7,8,8,10};
+	Solution soln;
+	vector<int>m=soln.searchRange(nums,8);
+	for(int i=0;i<m.size();i++)
+	{
+	    if(i==m.size()-1)
+	    {
+	        cout<<m[i];
+	    }
+	    else
+	    {
+	    cout<<m[i]<<",";
+	    }
+	}
+	return 0;
+}
