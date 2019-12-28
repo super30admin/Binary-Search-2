0adcd2f2c275bbdbfe926ac2b60b95fb1d91437a
rutulusc <rutulbap@usc.edu>
2019-12-28 13:34:18 -0800

    Problem 8 solved

---
+++ prob7.java
@@ -0,0 +1,10 @@
+// S30 Big N Problem #7 {Medium}
+// 34. Find First and Last Position of Element in Sorted Array
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :None
+
+
+// Your code here along with comments explaining your approach
+
---
+++ prob9.java
@@ -0,0 +1,9 @@
+// S30 Big N Problem #9 {Medium}
+// 162. Find Peak Element
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :None
+
+
+// Your code here along with comments explaining your approach
