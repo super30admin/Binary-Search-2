ec6121c34f75aa713f443629f5de0241ab691769
Venkata09578 <50789204+Venkata09578@users.noreply.github.com>
2019-12-29 01:03:16 -0800

    Update mininrotatedarray.py

--- mininrotatedarray.py
+++ mininrotatedarray.py
@@ -2,12 +2,7 @@
 Time Complexity: O(log n)
 Space complexity: O(n)
 Did the problem run on Leetcode?: Yes
-Problem: 
-A peak element is an element that is greater than its neighbors.
-Given an input array nums, where nums[i] ≠ nums[i+1], find a peak element and return its index.
-The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
-You may imagine that nums[-1] = nums[n] = -∞.
-"""
+
 
 
 class Solution:
