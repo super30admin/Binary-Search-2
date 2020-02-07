5fc2025ff7b2809ecfb1af5e05bd6e24f349c4eb
saiswaroopreddy <54874974+saiswaroopreddy@users.noreply.github.com>
2020-02-06 16:19:04 -0800

    Add files via upload

---
+++ Binary_Search_2.1.java
@@ -0,0 +1,3 @@
+//Find the target using binary search
+//Move left and check if mid is equal to mid -1 to get the first position and move right and check if mid +1 is equal to the target and return the last position
+//Do Until the bounding elements are found. 
