f0df962312ce16d2e42e4b47b2df9535576424ad
saiswaroopreddy <54874974+saiswaroopreddy@users.noreply.github.com>
2020-02-06 22:01:06 -0800

    Updated

--- Binary_search_2.2.java
+++ Binary_search_2.2.java
@@ -1,3 +1,23 @@
+//Time Complexity: O(logn)
+//Space Complexity: O(1)
+//leetcode: Yes
+
 //If value at mid is less the the value at mid -1 then return mid
 //If value at low > value at value at mid update high = mid -1
 //if value at high < value at mid update low  = mid +1
+
+class Solution {
+    public int findMin(int[] nums) {
+        if(nums == null || nums.length == 0){return -1;}
+        if(nums[0] < nums[nums.length-1]|| nums.length ==1)return nums[0];
+        int low =0, high = nums.length-1;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            
+            if(mid-1>0&&nums[mid]<nums[mid-1])return nums[mid];
+            if(mid+1 <= nums.length-1&&nums[mid]>nums[mid+1])return nums[mid+1];
+            if(nums[low]<nums[mid]){low = mid+1;}else{high = mid-1;}
+        }
+        return -1;
+    }
+}
