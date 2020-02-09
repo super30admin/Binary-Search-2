4c65d34a154c1674b8e78c82be292cb0bbe84dcd
rohank2002 <rohansantosh.kamat@sjsu.edu>
2020-02-08 18:33:29 -0800

    Passed all Test cases for Find the Peek Element Problem

--- Solutions/src/PeakElement.java
+++ Solutions/src/PeakElement.java
@@ -14,34 +14,15 @@ class PeakElement {
         if (nums==null || nums.length == 0){
             return -1;
         }
-        if (nums.length ==1){
-            return 0;
-        }
-        if (nums.length ==2){
-            if (nums[0]>nums[1]){
-                return 0;
-            }
-            else{
-                return 1;
-            }
 
-        }
-        if (nums.length ==3 && nums[0]<nums[1] && nums[1]>nums[2]){
-            return 1;
-        }
         int low = 0;
 
         int high = nums.length-1;
         while(low<=high){
             int mid = low + (high-low)/2;
-            // if(nums.length ==3 && nums[mid-1]>nums[mid] && nums[mid-1]>=nums[mid+1]){
-            //     return mid-1;
-            // }
-            // if(nums.length ==3 && nums[mid+1]> nums[mid] && nums[mid+1]>=nums[mid-1]){
-            //     return mid+1;
-            // }
+
             //Case 1: Peak element found
-            if ((mid+1<nums.length && (nums[mid-1]<nums[mid] && nums[mid]>nums[mid+1]) && mid -1>0)||(nums[mid]>nums[mid-1] && mid+1>nums.length) || (nums[mid]>nums[mid+1]) && mid-1 <0 ){
+            if ((mid-1<0 || nums[mid]> nums[mid-1]) && (mid+1>=nums.length || nums[mid]>nums[mid+1] )) {
                 return mid;
             }
 
