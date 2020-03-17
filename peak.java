b5276c54499e858d66a61d8735b86e3adfdeace6
kannan-mrugapphan <52872043+kannan-mrugapphan@users.noreply.github.com>
2020-03-17 16:45:32 -0500

    Create peak.java

---
+++ peak.java
@@ -0,0 +1,38 @@
+//Tiime - O(log n) and space is constant
+class Solution {
+    public int findPeakElement(int[] nums) {
+        if(nums.length == 1)
+        {
+            return 0;
+        }
+        if(nums.length == 2)
+        {
+            if(nums[0] > nums[1])
+            {
+                return 0;
+            }
+            return 1;
+        }
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;
+            int prev = (mid - 1 + nums.length) % nums.length;
+            int next = (mid + 1) % nums.length;
+            if(nums[prev] <= nums[mid] && nums[next] <= nums[mid])
+            {
+                return mid;
+            }
+            else if(nums[mid] < nums[prev])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
