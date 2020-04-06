6dc939e089b3e897e9a65db9440eb0d2d36f6bd8
Mrugapphan Kannan <52872043+kannan-mrugapphan@users.noreply.github.com>
2020-04-06 17:33:44 -0500

    Update FindMin.java

--- FindMin.java
+++ FindMin.java
@@ -1,33 +1,45 @@
-//time complexity - O(log n) - Modifying binary search
-//space cost - constant
 class Solution {
     public int findMin(int[] nums) {
         if(nums == null || nums.length == 0)
         {
             return -1;
         }
+        
         if(nums.length == 1)
         {
             return nums[0];
         }
+        
         int low = 0;
         int high = nums.length - 1;
+        
         while(low <= high)
         {
+            if(nums[low] < nums[high])
+            {
+                return nums[low];
+            }
+            
             int mid = low + (high - low) / 2;
-            if(mid > 0 && nums[mid] < nums[mid - 1])
+            int prev = (mid + nums.length - 1) % nums.length;
+            int next = (mid + 1) % nums.length;
+            
+            if(nums[mid] < nums[prev] && nums[mid] < nums[next])
             {
                 return nums[mid];
             }
-            else if(nums[low] <= nums[mid] && nums[mid] > nums[high])
+            
+            else if(nums[low] <= nums[mid])
             {
                 low = mid + 1;
             }
+            
             else
             {
-                high = mid - 1;            
-            }
+                high = mid - 1;
+            }    
         }
-        return nums[low];
+        
+        return -1;
     }
 }
