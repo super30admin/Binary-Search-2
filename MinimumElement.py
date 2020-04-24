d43c42b563326aa05eec15cc427406b84d82126e
Jacob Stephen <you@example.com>
2020-04-23 22:25:09 -0400

    Completed Design

---
+++ MinimumElement.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        start = 0
+        end =len(nums)-1
+        if nums[start] <= nums[end]:
+            return (nums[start])
+        else:
+            while(start<=end):
+                mid = (start + end)//2
+                if (nums[mid] > nums[mid+1]):
+                        return (nums[mid+1])
+                elif nums[start] < nums[mid] :
+                        start = mid
+                elif nums[mid] < nums[end]:
+                        end = mid
+                elif nums[start]> nums[mid] and nums[mid] <nums[end]:
+                        return (nums[mid])
+                
