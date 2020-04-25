8814d8b4b43082fcf72511b452fac37ad288e69c
HimanshuAgarwal93 <himanshu.agarwal93@yahoo.com>
2020-04-24 20:37:44 -0500

    PA7 PA8 & PA9 solutions

---
+++ PA9.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int low =0 ;
+        int high = nums.length-1;
+        
+        int mid=0;
+        while(low<=high){
+            mid= low+(high-low)/2;
+            if((mid==0 || nums[mid]>nums[mid-1]) && (mid==nums.length-1 || nums[mid]>nums[mid+1])){
+                return mid;
+            }
+               else{
+                   if(nums[mid]<nums[mid+1]){
+                       low=mid+1;
+                   }
+                   else{
+                       high=mid-1;
+                   }
+               }
+        }
+        return -1;
+    }
+}
