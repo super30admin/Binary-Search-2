b8e1a4b5106dddd31b939e31137b2167885bf190
Shivani Reddy <shivanireddy010197@gmail.com>
2020-05-27 23:10:38 -0700

    Completed

---
+++ MinInRotatedSortedArray.java
@@ -0,0 +1,22 @@
+// Time Complexity : O(log N) 
+// Space Complexity : O(1) 
+// Did this code successfully run on Leetcode : YYes
+// Any problem you faced while coding this : No
+
+class MinInRotatedSortedArray {
+    public int findMin(int[] nums) {
+        if(nums.length==1) return nums[0];
+        int low = 0; int high = nums.length - 1;
+        if(nums[high] > nums[0]) { // sorted with no rotation
+            return nums[0];
+        }
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(nums[mid] > nums[mid+1]) return nums[mid+1];
+            if(nums[mid-1] > nums[mid]) return nums[mid];
+            if(nums[mid] > nums[0]) low = mid + 1;
+            else high = mid - 1;
+        }
+        return -1;
+    }
+}
