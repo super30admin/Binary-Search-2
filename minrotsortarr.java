5de6751de6ca6560b10a734c592b37e909eda144
Pradhisha Rajendran <32206392+Pradhisha@users.noreply.github.com>
2020-05-27 22:21:32 -0400

    Add files via upload

---
+++ minrotsortarr.java
@@ -0,0 +1,40 @@
+// Time Complexity :O(log n)
+// Space Complexity :o(1) As array is not an Auxiliary  space
+// Did this code successfully run on Leetcode : N/A
+// Any problem you faced while coding this : -
+class Solution {
+    public int findMin(int[] nums) {
+        if(nums.length==1)
+            return nums[0];
+		// as it is rotated there will be two sorted subarrays in the array
+        int left=nums[0]; // getting min element of the first subarray
+        int right=binarySearchRight(nums); // getting the min element of the second sub array for which we dont know the starting index
+        return Math.min(left,right); // whichever is min in both will be returned
+    }
+
+    private int binarySearchRight(int nums[])
+    {
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2;
+            if(mid-1>=0 && nums[mid-1]<nums[end]) // if the mid is not the starting index of the subarray
+            {
+                end=mid-1;
+            }
+            else if(nums[mid]<=nums[end]) // if mid is the starting index of the subarray
+            {
+               
+                return nums[mid];
+            }
+ 
+            else{
+                start=mid+1; // if mid>end
+            }
+        }
+        
+      return -1;
+        
+    }
+}
---
+++ rangeinsortedarray.java
@@ -0,0 +1,72 @@
+// Time Complexity :O(log n)
+// Space Complexity :o(1) As array is not an Auxiliary  space
+// Did this code successfully run on Leetcode : N/A
+// Any problem you faced while coding this : -
+class Solution {
+    
+    //binary search for first index
+    private int binarySearchFirst(int[] nums,int target)
+    {
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2; // find mid
+            if(nums[mid]==target) // if mid matches target
+            {
+                if(mid==0 || nums[mid-1]<nums[mid]) // if mid is the first index in the array or if nums[mid] is the first occurrence
+                {
+                    return mid;
+                }
+                else
+                {
+                    end=mid-1; // if mid is not the first occurrence of the target,search the array before the mid index for first occurrence
+                }
+            }
+            else if(nums[mid] < target){
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return -1;
+    }
+    
+    
+     //binary search for last index
+    private int binarySearchLast(int[] nums,int target)
+    {
+        int start=0;
+        int end=nums.length-1;
+        while(start<=end)
+        {
+            int mid=start+(end-start)/2; // find mid
+            if(nums[mid]==target) // if mid matches target
+            {
+                if(mid==nums.length-1 || nums[mid+1]>nums[mid])// if mid is the last index in the array or if nums[mid] is the last occurrence
+                {
+                    return mid;             
+                }
+                else
+                    start=mid+1; // look for last occurrence after the mid
+            }
+            else if(nums[mid]<target)
+                start=mid+1;
+            else
+                end=mid-1;
+        }
+        return -1;
+        
+    }
+    
+    public int[] searchRange(int[] nums, int target) 
+    {
+        int start=0;
+        int end=nums.length-1;
+        if(nums==null || nums.length==0) return new int[]{-1,-1};
+        int first=binarySearchFirst(nums,target);
+        int last=binarySearchLast(nums,target);
+        return new int[]{first,last};
+    }
+}
