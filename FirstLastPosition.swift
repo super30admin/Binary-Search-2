d648783df7897ffa5470943d518717d4bfc6b6f0
karthikprabhu alagu <mail2karthi07@gmail.com>
2020-05-28 23:06:46 -0400

    After class.

--- FirstLastPosition.swift
+++ FirstLastPosition.swift
@@ -1,6 +1,68 @@
-//Not working for all test cases
+//After class
+// Time Complexity : (O)logn
+// Space Complexity : (O)1
+// Did this code successfully run on Leetcode : Yes
 
 class Solution {
+  func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
+
+      //edge case
+      if nums.count == 0 {
+          return [-1,-1]
+      }
+      
+      let first: Int = binarySearchLeft(nums, target)
+      let last: Int = binarySearchRight(nums, target)
+       
+      return [first,last]
+  }
+    func binarySearchRight(_ nums: [Int], _ target: Int) -> Int {
+        var low = 0
+        var high = nums.count - 1
+        while(low <= high) {
+            var mid = low + (high - low)/2
+            if nums[mid] == target { 
+               if mid == nums.count - 1 || nums[mid] < nums[mid+1] {
+                 return mid
+               } else {
+                   low = mid + 1
+               }
+            }else if nums[mid] > target {
+                   high = mid - 1
+               } else {
+                   low = mid + 1
+             }
+        }
+        return -1
+    }
+    
+    func binarySearchLeft(_ nums: [Int], _ target: Int) -> Int {
+        var low = 0
+        var high = nums.count - 1
+        while(low <= high) {
+            var mid = low + (high - low)/2
+            if nums[mid] == target { 
+               if mid == 0 || nums[mid] > nums[mid-1] {
+                 return mid
+               } else {
+                  high = mid - 1 
+               }
+            }else if nums[mid] > target {
+                   high = mid - 1
+             } else {
+                   low = mid + 1
+            }
+        }
+        return -1
+    }
+    
+}
+
+
+//Not working for all test cases. Will try to fix and commit later.
+
+
+/*class Solution {
   func searchRange(_ nums: [Int], _ target: Int) -> [Int] {
 
       
@@ -53,3 +115,4 @@ class Solution {
 
     
 }
+*/
