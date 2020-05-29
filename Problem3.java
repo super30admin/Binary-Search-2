80129279064e72cfc4d264c843345572ec404280
saumilnpatel90 <56368073+saumilnpatel90@users.noreply.github.com>
2020-05-29 05:14:47 -0700

    Create Problem3.java

---
+++ Problem3.java
@@ -0,0 +1,25 @@
+
+public class Solution {
+	public int findPeakElement(int[] nums) {
+	    
+		int low = 0, high = nums.length - 1;
+		
+        if(low > high) return -1;
+		return binarySearch(nums, low, high);
+    }
+	
+	private int binarySearch(int[] nums, int low, int high) {
+		
+		int mid = low + (high - low)/2;
+		
+        if(mid < high && nums[mid] < nums[mid+1]){
+            low = mid + 1;
+            return binarySearch(nums, low, high);
+        } else if(mid > low && nums[mid] < nums[mid-1]){
+            high = mid - 1;
+            return binarySearch(nums, low, high);
+        } else {
+            return mid;
+        }
+	}
+}
