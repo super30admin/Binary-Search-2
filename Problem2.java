21e5f25e4813ba3c459ba9a4f0319f3139f86f1e
saumilnpatel90 <56368073+saumilnpatel90@users.noreply.github.com>
2020-06-01 06:36:38 -0700

    Update Problem2.java

--- Problem2.java
+++ Problem2.java
@@ -1,3 +1,7 @@
+// Time Complexity : O(log n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 public class Solution {
 
