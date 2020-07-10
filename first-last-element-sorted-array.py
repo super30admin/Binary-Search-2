74a49d8094d8f2e357489b65d2de01bf63639698
Arpit Sharma <arpit317@gmail.com>
2020-07-10 02:18:21 -0700

    Update first-last-element-sorted-array.py

--- first-last-element-sorted-array.py
+++ first-last-element-sorted-array.py
@@ -1,3 +1,16 @@
+# Time Complexity : O(log(n)) 
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : yes
+# Any problem you faced while coding this : No
+
+#Approach:
+#1. We will use binary search. This is the iterative solution for binary search.
+#2. We use binary search for both the first and last element one by one. For finding the first element, if we find the target at the mid element of the array,
+# we compare it with its left neighbor and check that either it should be leftmost element or it should be larger than its left neighbor. 
+# If it is so, we return mid, or else we move towards left subarray.
+#3. Similarly, to find the last element, we compare the matching target element with its right neighbor, if it's the rightmost element or it is smaller than its
+# right neighbor, then it is our last element. If it is so, we return mid, or else we move towards the right subarray.
+
 class Solution(object):
     def searchRange(self, nums, target):
         """
@@ -5,6 +18,7 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        
         def first(nums, target, start, end):
             if start > end: return -1
             while start <= end:
@@ -32,6 +46,7 @@ class Solution(object):
                 else:
                     end = mid - 1
             return -1
+        
         f = first(nums, target, 0, len(nums)-1)
         s = second(nums, target, 0, len(nums)-1)
         return [f,s]
