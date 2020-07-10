d1e6a43675ea880fae4a355fcd159d6ddaa3a4e2
Arpit Sharma <arpit317@gmail.com>
2020-07-10 02:10:48 -0700

    Update find-peak-element.py

--- find-peak-element.py
+++ find-peak-element.py
@@ -1,3 +1,15 @@
+# Time Complexity : O(log(n)) 
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : yes
+# Any problem you faced while coding this : No
+
+#Approach:
+#1. We will use binary search. This is the iterative solution for binary search.
+#2. We check if the mid element of the array is peak element by comparing it with its neighbors, if it is, we return its index or else, we take that part of the 
+# array towards which we see a larger element than the mid.
+#3. If both the left and right neighbors are larger, any of the sub array can be taken for next iteration.
+
+
 class Solution(object):
     def findPeakElement(self, nums):
         """
