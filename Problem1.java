f9174ba05b2eb62f47b11662bb403e597aba0655
soumil.sarvaiya <ss8165@rit.edu>
2020-07-10 16:07:07 -0400

    Completed Problem1

--- Problem1.java
+++ Problem1.java
@@ -1,7 +1,75 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity :O(log n)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this : if array contains only two same elements
 
 
 // Your code here along with comments explaining your approach
+class Problem1 {
+
+    /** Search for left and right location of target */
+    public int[] searchRange(int[] nums, int target) {
+        
+        // Search left
+        int left = binarySearchLeft(nums, 0, nums.length-1, target);
+        
+        // if not found
+        if(left ==-1){
+            return new int[]{left,left};
+        }
+        // Search for right
+        else{
+            // using previously calculated left
+            int right = binarySearchRight(nums, left, nums.length-1, target);
+            return new int[] {left, right};
+        }
+        
+    }
+    
+    int binarySearchLeft(int[] nums,int low, int high, int target){
+        
+        while(low<=high){
+            
+            // find mid
+            int mid = low + (high-low)/2;
+            
+            if(nums[mid] == target){
+                // mid should be greater than low for array out of bounds
+                if(mid > low && nums[mid-1] == target)
+                    high = mid-1;
+                else
+                    return mid;
+                
+            }else if(nums[mid] < target){
+                low = mid +1;
+            }else{
+                high = mid -1;
+            }
+        }
+        // not found
+        return -1;
+    }
+    
+    int binarySearchRight(int[] nums, int low, int high, int target){
+        
+         while(low<=high){
+
+            int mid = low + (high-low)/2;
+             
+            if(nums[mid] == target){
+                // mid should be less than high for array out of bounds
+                if(mid < high && nums[mid+1] == target)
+                    low = mid+1;
+                else
+                    return mid;
+                
+            }else if(nums[mid] < target){
+                low = mid +1;
+            }else{
+                high = mid -1;
+            }
+        } 
+        // not found
+        return -1;
+    }
+}
