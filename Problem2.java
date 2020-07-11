6606d324fcfbc90afd0ad90370992a10e1ac3ffb
soumil.sarvaiya <ss8165@rit.edu>
2020-07-10 20:58:13 -0400

    Completed Problem2

--- Problem2.java
+++ Problem2.java
@@ -1,7 +1,37 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity :O(log n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : edge cases
 
 
 // Your code here along with comments explaining your approach
+
+class Problem2 {
+    /** find minimum */
+    public int findMin(int[] nums) {
+
+        int low = 0;
+        int high =  nums.length-1;
+        
+        while(low<=high){
+            
+            // mid
+            int mid = low + (high-low)/2;
+
+            // if array is sorted return 1st element
+            if(nums[low]< nums[high])
+                return nums[low];
+            // using the pivot property
+            if((mid==low || nums[mid]<nums[mid-1]) && (mid==high || nums[mid]<nums[mid+1])){
+                    return nums[mid];
+            // one element
+            }else if(nums[low] <=nums[mid]){
+                low = mid +1;
+            }else{
+                high = mid -1;
+            }
+        }
+        
+        return -1;
+    }
+}
