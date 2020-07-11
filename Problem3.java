591e41c7d8f670a664edc68aea4b58649d3cae8f
soumil.sarvaiya <ss8165@rit.edu>
2020-07-10 21:17:01 -0400

    Completed Problem3

--- Problem3.java
+++ Problem3.java
@@ -1,7 +1,25 @@
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity :O(log n)
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : edge cases
 
 
 // Your code here along with comments explaining your approach
+class Problem3 {
+    public int findPeakElement(int[] nums) {
+        
+        int low = 0;
+        int high =  nums.length-1;
+        
+        while(low<high){
+            int mid =  low + (high -low)/2;
+            if(nums[mid] > nums[mid+1])
+                high =  mid;
+            else
+                low = mid+1;
+        }
+        
+        return low;
+    }
+
+}
