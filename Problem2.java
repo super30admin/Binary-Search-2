0328197b52ddd9b7f9b5d71e367d5b05e557c5fd
Nidhi Aravind Kumar <nidhi07.arvind@gmail.com>
2020-08-21 15:04:26 -0700

    Solved problem3

--- Problem2.java
+++ Problem2.java
@@ -1,3 +1,8 @@
+// Time Complexity : O(logn) 
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
 class Solution{
 
     public static int minValue(int[] a){
