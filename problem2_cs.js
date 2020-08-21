2e926cd6585d77160bfb9a3e0872e9727c71bcc0
Ken Ore <v-kore@expediagroup.com>
2020-08-20 23:21:57 -0500

    problem2 now working and passing on leetcode

--- problem2_cs.js
+++ problem2_cs.js
@@ -1,5 +1,32 @@
+// Time Complexity : O(lg n)
+// Space Complexity : O(1) no additional space
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this :
+//   mental hurdle: choosing the side with the dip is a mental leap
+
+// Approach:
+// Do a binary search on nums
+// Min element will be the mid such that nums[mid-1] > nums[mid] < nums[mid+1]
+// if mid isn't min element
+//   go right if nums[mid] > nums[high] (because interval [mid, high] is out of order and contains the dip)
+//   else go left
 var findMin;
 
-findMin = function(nums) {};
+findMin = function(nums) {
+  var high, low, mid, ref;
+  low = 0;
+  high = nums.length - 1;
+  while (low <= high) {
+    mid = Math.floor(low + (high - low) / 2);
+    if ((nums[mid - 1] > (ref = nums[mid]) && ref < nums[mid + 1])) {
+      return nums[mid];
+    } else if (nums[mid] > nums[high]) {
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+  return -1;
+};
 
 //# sourceMappingURL=problem2_cs.js.map
---
+++ problem3_cs.js
@@ -0,0 +1,9 @@
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+// Approach:
+
+
+//# sourceMappingURL=problem3_cs.js.map
