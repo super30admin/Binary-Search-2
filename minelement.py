4bbcf64d626ec7f0cc16fdff1a3f61b87007302a
Damanpreet Kaur <damanpreetkaur@Damanpreets-MacBook-Pro.local>
2020-08-20 21:56:17 -0700

    Modified

--- minelement.py
+++ minelement.py
@@ -24,3 +24,31 @@ class Solution:
                 high = mid 
             
         return high
+
+
+# Using binary search
+# Time complexity - O(log n)
+# Space complexity - O(1)
+# Did this solution run on leetcode? - yes
+class Solution:
+    def findMin(self, nums: List[int]) -> int:    
+        """
+            - Compare the left and right subarrays.
+            - Consider the right subarray whether if it sorted.
+            - If it is sorted, move the high pointer to mid-1.
+            - else (right is unsorted), move the low pointer to mid+1.
+            - when the mid element is the smallest, return it.
+        """
+        n = len(nums)
+        low, high = 0, n-1
+        
+        while low<=high:
+            mid = low + (high-low)//2
+            if (mid==0 or nums[mid] < nums[mid-1]) and (mid==n-1 or nums[mid] < nums[mid+1]):
+                return nums[mid]
+            elif nums[high] > nums[mid]: # left subarray is sorted.
+                high = mid - 1
+            else:                        # right subarray is sorted.
+                low = mid + 1
+            
+        return -1
