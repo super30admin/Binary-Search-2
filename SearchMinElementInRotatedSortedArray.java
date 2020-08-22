0f5fb96001ebf7d3b23798436453e390f4507f25
rupeshbiradar40 <rupeshbiradar40@gmail.com>
2020-08-22 15:44:20 -0500

    Binary Search

--- SearchMinElementInRotatedSortedArray.java
+++ SearchMinElementInRotatedSortedArray.java
@@ -6,31 +6,20 @@
 
 // Your code here along with comments explaining your approach
 class Solution {
-    public int search(int[] nums, int target){
+    public int findMin(int[] nums) {
         int start=0;
-        int end = nums.length-1;
+        int end=nums.length-1;
         
         while(start<=end){
-            int mid = start + (end-start)/2;
-            
-            if(nums[mid]==target){
-                return mid;
-            }else if(nums[mid]>=nums[start]){
-                
-                if(nums[mid]>target && nums[start]<=target){
-                    end=mid-1;
-                }else{
-                    start=mid+1; 
-                }        
-            }
-            else{
-                
-                if(nums[mid]<target && nums[end]>=target){
-                    start=mid+1;
-                }else{
-                    end=mid-1;   
-                }      
-            }  
+            int mid = start +(end-start)/2;
+            if((mid==0 || nums[mid]<nums[mid-1]) && (mid==nums.length-1 || nums[mid]<nums[mid+1])){
+               return nums[mid]; 
+            }else if(nums[mid]>nums[end]){
+                 start=mid+1;  
+            }else{
+                end=mid-1;
+           }        
         }
         return -1;
     }
+}
