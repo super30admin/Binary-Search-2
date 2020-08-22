b98d2ef2109d1f8ff6731921f590cfedc6cd0adb
Vishal kumar M G <vishalkumarmg@Vishals-MacBook-Pro.local>
2020-08-22 17:40:32 -0500

    Completed problems in Binary-search-2

---
+++ Problem_2.py
@@ -0,0 +1,25 @@
+# 153. Find Minimum in Rotated Sorted Array
+
+# Approach: Given sorted array, so Binary search is possible.
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        low = 0
+        high = len(nums)-1
+        
+        while low<=high:
+            mid = low + (high-low)//2
+            
+            if ((mid==len(nums)-1 or nums[mid]<nums[mid+1]) and (mid==0 or nums[mid]<nums[mid-1])):
+                return nums[mid]
+            
+            elif nums[mid]>=nums[high]:
+                low = mid+1
+            else:
+                high = mid-1
+            
+        
+        return -1
+
+# Time Complexity: O(logN)
+# Space Complexity: O(1)
