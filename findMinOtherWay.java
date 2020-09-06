ea215ea06f4d3805cdc3ac25385cdf1c566dca15
Prashant Kariyannavar <kprashantprabhu93@gmail.com>
2020-09-06 23:23:48 +0530

    Find Minimum in other way

---
+++ findMinOtherWay.java
@@ -0,0 +1,18 @@
+
+public class findMinOtherWay {
+	public int findMin(int[] nums) {
+
+		int low = 0;
+		int high = nums.length - 1;
+		while (low < high) {
+			int mid = low + (high - low) / 2;
+			if (nums[mid] > nums[high])
+				low = mid + 1;
+			else
+				high = mid;
+
+		}
+		return nums[low];
+	}
+
+}
