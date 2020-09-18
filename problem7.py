2ac5be59e366ea4613feab7989c9eb552d868aa6
Jeevtesh Singh <50637812+jeevy222@users.noreply.github.com>
2020-09-18 10:37:08 -0400

    Create problem7.py

---
+++ problem7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def searchRange(self, nums, target):
+        def binarySearchLeft(A, x):
+            left, right = 0, len(A) - 1
+            while left <= right:
+                mid = int((left + right) / 2)
+                if x > A[mid]: left = mid + 1
+                else: right = mid - 1
+            return left
+
+        def binarySearchRight(A, x):
+            left, right = 0, len(A) - 1
+            while left <= right:
+                mid = int((left + right) / 2)
+                if x >= A[mid]: left = mid + 1
+                else: right = mid - 1
+            return right
+
+        left, right = binarySearchLeft(nums, target), binarySearchRight(nums, target)
+        return (left, right) if left <= right else [-1, -1]
