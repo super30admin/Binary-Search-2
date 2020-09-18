e8be1c26aa24cd89c74183f9cc013f6cfbc5719c
Harsha Vardhan <harshavardhan@Harshas-MacBook-Air.local>
2020-09-17 20:40:17 -0700

    Assignment 4

---
+++ min_element_sorted_rotated_array.py
@@ -0,0 +1,18 @@
+# O(log(n)) time and O(1) space
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+        while left < right:
+            mid = (left + right) // 2
+            if nums[mid] < nums[right]:
+                right = mid
+            else:
+                left = mid+1
+        return nums[left]
+                
+        
