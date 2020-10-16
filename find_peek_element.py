aedf1f150fb4b03cea7b57a29b035d57862ef862
GayatriHungund81194 <gayatri811.gh@gmail.com>
2020-10-16 00:20:57 -0700

    Modified approaches as taught in class

--- find_peek_element.py
+++ find_peek_element.py
@@ -1,14 +1,20 @@
+# As taught in the class, I used modified approach with binary search where I chekc elements to both left and right of mid to return the peak
+#Time complexity: O(log(n))
+#Space Complexity: O(n)
 def findPeek(arr):
-    peekarr = list()
-    if arr is None:
+    low = 0
+    high = len(arr) - 1
+    if (arr is None or len(arr) == 0):
         return -1
-    if len(arr)==1:
-        return arr
-    for i in range(1,len(arr)-1):
-        if (i!=0 and arr[i] is not None and arr[i-1] is not None and arr[i+1] is not None):
-            if (arr[i-1]<arr[i] and arr[i+1]<arr[i]):
-                peekarr.append(arr[i])
-    return peekarr
+    while (low <= high):
+        mid = int((low + high)/2)
+        if ((mid == 0 or arr[mid]>arr[mid-1]) and ( mid== len(arr)-1 or arr[mid]> arr[mid + 1])):
+            return mid
+        elif (mid>0 and arr[mid]<arr[mid-1]):
+            high = mid - 1
+        else:
+            low = mid  + 1 
+    return -1
 
 arr = [1,2,1,3,5,6,4]
 arr = findPeek(arr)
--- find_rotated_sorted_array.py
+++ find_rotated_sorted_array.py
@@ -1,31 +1,27 @@
-def binarySearch(arr, l, r, x): 
-    #write your code here
-    print(l)
-    print(r)
-    print(x)
-    while(l<=r):
-        mid = int((l+r)/2)
-        if (arr[mid]==x):
-            return mid
-        elif (x>arr[mid]):
-            #search on the right side of the middle element
-            l = mid + 1
-        else:
-            r = mid - 1
-    return -1
-
-def search(arr,target):
+#The easier way is to solve the problem linearly but the time complexity can reduced by using binary search.
+#The approach I used here is first find mid if prev element is less than mid then mid is lowest or if mid+1 is less than mid then mid+1 element is lowest
+#But, if 1st element of array is less than mid then search to right but if its greater rearch to left
+# Time complexity O(log(n))
+# Space Complexity O(n)
+def search(arr):
     low = 0
-    high = len(arr)
-    mid = int((low+high)/2)
-    print(arr[mid])
-    if (arr[mid]>target):
-        idx = binarySearch(arr,low,mid,target)
-        return idx
-    else:
-        idx = binarySearch(arr,mid+1,high,target)
-        return idx
+    high = len(arr)-1
+    if (len(arr)==0):
+        return -1
+    if len(arr) == 1:
+        return arr[0]
+
+    while (low <= high):
+        mid = int((low+high)/2)
+        if (arr[mid] > arr[mid+1]):
+            return arr[mid+1]
+        elif (arr[mid-1] < arr[mid]):
+            return arr[mid]
+        if arr[mid] > arr[low]:
+            low = mid + 1
+        else:
+            high  = mid - 1
 
 arr = [4,5,6,7,0,1,2]
-index = search(arr,5)
-print(index)
+index = search(arr)
+print(index) 
