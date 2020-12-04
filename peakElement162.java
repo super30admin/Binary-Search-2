b634c2621521bbbb31c0a084411211bbe7e4af77
Shivi_Bhatt <in.shivi@gmail.com>
2020-12-03 16:20:02 -0800

    Problem - 3 Completed

---
+++ peakElement162.java
@@ -0,0 +1,21 @@
+package nov192020;
+
+// Problem 9 
+// https://leetcode.com/problems/find-peak-element/
+// https://github.com/super30admin/Binary-Search-2
+public class peakElement162 {
+	public int findPeakElement(int[] nums) {
+		int low = 0;
+		int high = nums.length - 1;
+		while (low < high) {
+			int mid = (low + high) / 2;
+			if (nums[mid] > nums[mid + 1]) {
+				high = mid;
+			} else {
+				low = mid + 1;
+			}
+		}
+		return low;
+	}
+
+}
