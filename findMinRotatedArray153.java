802122359d9c4be03cae44995e00ac61526fb2ae
Shivi_Bhatt <in.shivi@gmail.com>
2020-12-08 11:47:07 -0800

    Problem 2 done

---
+++ findMinRotatedArray153.java
@@ -0,0 +1,29 @@
+package nov192020;
+
+// Problem 8
+// Source Link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+// https://github.com/super30admin/Binary-Search-2
+public class findMinRotatedArray153 {
+	public int findMin(int[] nums) {
+		if (nums == null || nums.length == 0)
+			return 0;
+		int low = 0;
+		int high = nums.length - 1;
+		while (low <= high) {
+			int mid = low + (high - low) / 2;
+			if (nums[low] < nums[high]) {
+				return nums[low];
+			}
+			if ((mid == low || nums[mid] < nums[mid - 1]) && (mid == high || nums[mid] < nums[mid + 1])) {
+				return nums[mid];
+			} else if (nums[low] <= nums[mid])
+				low = mid + 1;
+			else {
+				high = mid - 1;
+			}
+
+		}
+		return -1;
+	}
+
+}
