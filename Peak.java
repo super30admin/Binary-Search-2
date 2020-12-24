8aef3d83191a4e91efd388947d23bf9bb855746a
Chetan Dhowlaghar <dhowlaghar.c@northeastern.edu>
2020-12-24 18:11:29 -0500

    Peak problem solved

---
+++ Peak.java
@@ -0,0 +1,21 @@
+class Peak {
+     public int findPeakElement(int[] nums) {
+        
+        int high = nums.length;
+        int low = 0;
+        int x= findpeak(high,low,nums);
+         return x;
+     }
+        
+        
+     public int findpeak( int high, int low, int[] nums)
+        {
+           int mid= low+ (high-low)/2;
+           
+            if(nums[mid]>=nums[mid+1] && nums[mid]>=nums[mid+1]) return mid;
+            else if (nums[mid-1]>nums[mid]) return findpeak(mid-1, low, nums);
+           else return findpeak(high, mid +1, nums);
+        }
+        
+    
+}
