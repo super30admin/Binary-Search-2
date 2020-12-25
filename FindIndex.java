8686e4065e47bf4b21d7df5097bcaa5df098e2fe
Chetan Dhowlaghar <dhowlaghar.c@northeastern.edu>
2020-12-24 21:52:22 -0500

    Stuck at find index

---
+++ FindIndex.java
@@ -0,0 +1,17 @@
+public class FindIndex {
+    
+    public int[] searchRange(int[] nums, int target) {
+            
+        int output[]= new  int[2];
+        int high= nums.length-1;
+        int low= 0;
+
+
+
+
+
+
+        return output;
+    }    
+    
+}
