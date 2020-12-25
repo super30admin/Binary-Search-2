b37e39642b89fccf1c076700c36ea79a7e3cd993
Sravanthi Pasumarthi <sravanthi.pasumarthi1@gmail.com>
2020-12-24 20:06:16 -0600

    adding solutions for sorted array variants

---
+++ Solution_Sorted_min.java
@@ -0,0 +1,45 @@
+class Solution {
+  public int findMin(int[] nums) {
+  
+    if (nums.length == 1) {
+      return nums[0];
+    }
+
+   
+    int left = 0, right = nums.length - 1;
+
+    // if the last element is greater than the first element then there is no rotation.
+    
+    if (nums[right] > nums[0]) {
+      return nums[0];
+    }
+    
+    // Binary search way
+    while (right >= left) {
+      // Find the mid element
+      int mid = left + (right - left) / 2;
+
+      // if the mid element is greater than its next element then mid+1 element is the smallest
+      if (nums[mid] > nums[mid + 1]) {
+        return nums[mid + 1];
+      }
+
+      // if the mid element is lesser than its previous element then mid element is the smallest
+      if (nums[mid - 1] > nums[mid]) {
+        return nums[mid];
+      }
+
+      // if the mid elements value is greater than the 0th element this means
+      // the least value is still somewhere to the right as we are still dealing with elements
+      // greater than nums[0]
+      if (nums[mid] > nums[0]) {
+        left = mid + 1;
+      } else {
+        // if nums[0] is greater than the mid value then this means the smallest value is somewhere to
+        // the left
+        right = mid - 1;
+      }
+    }
+    return -1;
+  }
+}
