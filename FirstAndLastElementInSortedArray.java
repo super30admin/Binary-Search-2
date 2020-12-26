3cbb3f9b89362eafbee7ba81eda2505902c0b09b
abhisheksharma0904 <76174342+abhisheksharma0904@users.noreply.github.com>
2020-12-26 22:09:15 +0530

    Create FirstAndLastElementInSortedArray.java

---
+++ FirstAndLastElementInSortedArray.java
@@ -0,0 +1,53 @@
+class Solution {
+    private int binarySearchFirst(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = low + ( high - low) / 2;
+            if(nums[mid] == target){
+                if(mid == 0 || nums[mid] > nums[mid-1]){
+                    return mid;
+                }
+                else{
+                    high = mid - 1;
+                }                
+            }
+            else if(nums[mid] > target){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+    private int binarySearchLast(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            int mid = low + ( high - low) / 2;
+            if(nums[mid] == target){
+                if(mid == nums.length - 1 || nums[mid] < nums[mid+1]){
+                    return mid;
+                }
+                else{
+                    low = mid + 1;
+                }                
+            }
+            else if(nums[mid] > target){
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+    public int[] searchRange(int[] nums, int target) {
+        if(nums == null || nums.length == 0) return new int[] {-1, -1};
+        if(nums[0] > target || nums[nums.length - 1] < target) return new int [] {-1, -1};
+        int first = binarySearchFirst(nums, target);
+        int last = binarySearchLast(nums, target);
+        return new int[] {first, last};
+    }
+}
