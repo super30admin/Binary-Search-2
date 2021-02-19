e3f05c6523dbbe1c7a5e24dcfe277e625c7840db
shardul pathak <shardul94pathak@gmail.com>
2021-02-18 23:50:22 -0500

    Optimized Binary-Search-2 FirstAndLastPosition

--- FirstAndLastPosition.java
+++ FirstAndLastPosition.java
@@ -39,3 +39,108 @@ class Solution {
         return result;
     }
 }
+
+
+
+
+
+
+
+//Time Complexity : O(log n)
+//Space Complexity : O(1)
+//Did this code successfully run on Leetcode : Yes
+//Any problem you faced while coding this : No
+
+
+//Your code here along with comments explaining your approach
+//here we are using two binary searches.
+//We use one for searching towards the left the first occurence of target
+//WE use the other for searching towards right the last occurence of target
+
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        
+        if(nums == null || nums.length == 0 || target<nums[0] || target > nums[nums.length-1])
+        {
+          return new int[]{-1,-1};
+        }
+        
+        int start = leftBinarySearch(nums,target);
+        int end = rightBinarySerach(nums,target);
+        
+        return new int[]{start,end};
+       
+    }
+    //searching to the left first occurence
+    private int leftBinarySearch(int[] nums, int target)
+    {
+    	//regular binary search with just a condition to stop when no more elements to the left are same as target
+        int l = 0;
+        int r = nums.length - 1;
+        
+        while(l <= r)
+        {
+            int mid = l + (r-l)/2;
+            
+            if(target == nums[mid])
+            {
+            	//if we find there are no more same elements as target to the left
+                if(mid == 0 || nums[mid]>nums[mid-1])
+                {
+                    return mid;
+                }
+                else
+                {
+                    r = mid -1;
+                }
+            }
+            
+            else if(target<nums[mid])
+            {
+                r = mid -1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
+        }
+        return -1;
+    }
+    
+    private int rightBinarySerach(int[] nums, int target)
+    {
+        int l = 0;
+        int r = nums.length -1 ;
+    	//regular binary search with just a condition to stop when no more elements to the right are same as target
+
+        while(l <= r)
+        {
+            int mid = l + (r-l)/2;
+            
+            if(target == nums[mid])
+            {
+            	//if we find there are no more same elements as target to the right
+
+            if( mid == nums.length-1 || nums[mid]<nums[mid+1])
+            {
+                return mid;
+            }
+            else
+            {
+                l = mid + 1;        
+            }
+            }
+            else if (target > nums[mid])
+            {
+                l = mid + 1;
+            }
+            else
+            {
+                r = mid -1 ;
+            }
+            
+        }
+        return -1;
+    }
+}
