1e79b13c50cab0872b868543e47e980ca98c4ca3
shardul pathak <shardul94pathak@gmail.com>
2021-02-18 23:05:59 -0500

    Optimized solution Binary-Search-2 MinRotatedArray

--- MinRotatedArray.java
+++ MinRotatedArray.java
@@ -54,3 +54,54 @@ class Solution {
         return -1;
     }
 }
+
+//Time Complexity : O(log n)
+//Space Complexity : O(1)
+//Did this code successfully run on Leetcode : Yes
+//Any problem you faced while coding this : No
+
+
+//Your code here along with comments explaining your approach
+//This is from the apporoach discussed in class , basically we check if element at mid is greater than element at r that means we have to search
+//in right side of mid
+//and we return mid when the element before mid is smaller than mid and element after mid is greater than mid
+
+class Solution {
+ public int findMin(int[] nums) {
+     
+     
+     int l = 0;
+     int r = nums.length-1;
+     //if is it already a sorted array
+     if(nums[l]<nums[r])
+     {
+         return nums[l];
+     }
+     //if array has only one element
+     if(nums.length==1)
+     {
+         return nums[l];
+     }
+     while(l <= r)
+     {
+         int mid = l +(r-l)/2;
+         
+         //checking mid if it is smaller than both previous and next , also accomodating boundary conditions
+         if((mid==0 || nums[mid]<nums[mid-1]) && (mid == nums.length-1 || nums[mid]<nums[mid+1]))
+         {
+        	 return nums[mid];
+         }
+         //this case represents that min is at right side of mid we can disregard the left side
+         if(nums[mid]>nums[r])
+         {
+        	 l = mid + 1;
+         }
+         else
+         {
+        	 r = mid -1;
+         }
+         
+     }
+     return -1;
+ }
+}
