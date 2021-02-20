fee254f43c7cde999ad1f97938ce08b68077f1ec
Sai Manudeep Gadde <38970629+Manudeep-git@users.noreply.github.com>
2021-02-19 21:53:12 -0600

    Update Peak_element.java

--- Peak_element.java
+++ Peak_element.java
@@ -1,21 +1,25 @@
  // Time Complexity : O(logn) where n is  number of rows, m is no of columns
 // Space Complexity : O(1) 
 // Did this code successfully run on Leetcode : Yes
-// Any problem you faced while coding this : Couldn't understand what to return, had to see the solution
-
+// Any problem you faced while coding this : No
 class Solution {
     public int findPeakElement(int[] nums) {
-        int left = 0, right = nums.length-1;
-        
-        while(left < right){
-            int mid = left+(right-left)/2;
-            if(nums[mid] < nums[mid+1]){
-                left = mid+1;
-            } else {
-                right = mid;
-            }
+        if(nums.length==1){
+            return 0;
         }
         
-        return left;
+        for(int i=1;i<nums.length-1;i++){
+            if((nums[i-1]<nums[i]) && (nums[i]>nums[i+1])){
+                return i;
+            }
+        }
+        if(nums[0]>nums[1]){
+            return 0;
+        }
+        
+        if(nums[nums.length-1]>nums[nums.length-2]){
+            return nums.length-1;
+        }
+        return -1;
     }
 }
