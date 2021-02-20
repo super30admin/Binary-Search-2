774cca7be19925c7ba868d12a48e01e32da99935
Sai Manudeep Gadde <38970629+Manudeep-git@users.noreply.github.com>
2021-02-20 00:34:46 -0600

    Update Peak_element.java

--- Peak_element.java
+++ Peak_element.java
@@ -1,4 +1,6 @@
- // Time Complexity : O(logn) where n is  number of rows, m is no of columns
+------------------------------------------------------------------ Approach 1 ----------------------------------------------------------
+
+// Time Complexity : O(n)
 // Space Complexity : O(1) 
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
@@ -21,5 +23,34 @@ class Solution {
             return nums.length-1;
         }
         return -1;
+    }
+}
+
+-------------------------------------------------------------------- Approach 2 -----------------------------------------------------------------------------
+ // Time Complexity : ~O(logn)
+// Space Complexity : O(1) 
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int low=0;
+        int high = nums.length-1;
+            
+        while(low<=high){
+            int mid= low +(high-low)/2;
+            
+            if((mid==0 || nums[mid-1]<nums[mid]) && (mid== nums.length-1 || nums[mid+1]< nums[mid])){
+                return mid;
+            }
+            else{
+                if(nums[mid]<nums[mid+1]){
+                    low=mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+            }
+        }
+          return -1;
     }
 }
