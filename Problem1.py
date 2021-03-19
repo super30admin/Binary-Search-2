ce5e7dfe7c5318a5860f0f5c691f2318536a0eae
Vishnu <46506049+vp43@users.noreply.github.com>
2021-03-19 15:33:12 -0500

    Completed Sprint Binary-Search-2

--- Problem1.py
+++ Problem1.py
@@ -1,1 +1,51 @@
+# Find First and Last Position of Element in Sorted Array
+''' Time complexity : O(log N)
+Space Complexity: O(1)
 
+
+Did this code successfully run on Leetcode : yes
+Any problem you faced while coding this : no '''
+
+class Solution:
+    
+    def binarySearchFirst(self,nums: List[int],target: int):
+        low=0
+        high = len(nums)-1
+        while(low<=high):
+            mid = low + (high-low)//2
+            if target==nums[mid]:
+                if (mid ==0 or nums[mid-1]<nums[mid]):
+                    return mid
+                elif nums[mid-1]==nums[mid]:
+                    high = mid-1
+            elif target > nums[mid]:
+                low = mid +1
+            else:
+                high = mid-1
+        return -1
+    
+    def binarySearchLast(self,nums: List[int],target: int):
+        low=0
+        high = len(nums)-1
+        while(low<=high):
+            mid = low + (high-low)//2
+            if target==nums[mid]:
+                if (mid ==len(nums)-1 or nums[mid+1]>nums[mid]):
+                    return mid
+                elif nums[mid+1]==nums[mid]:
+                    low = mid+1
+            elif target > nums[mid]:
+                low = mid +1
+            else:
+                high = mid-1
+        return -1
+    
+    
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        if len(nums)==0:
+            return[-1,-1]
+        if (target<nums[0] or target>nums[len(nums)-1]):
+            return[-1,-1]
+        first = self.binarySearchFirst(nums,target)
+        second = self.binarySearchLast(nums,target)
+        return [first,second]
--- Problem3.py
+++ Problem3.py
@@ -1,1 +1,22 @@
+# Python program to Find Peak Element
+''' Time complexity : O(log N)
+Space Complexity: O(1)
 
+
+Did this code successfully run on Leetcode : yes
+Any problem you faced while coding this :no '''
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        low = 0
+        high = len(nums) - 1
+        while low <=high:
+            mid = (low + high) // 2
+            if ((mid == low or nums[mid] > nums[mid - 1]) and (mid== len(nums)-1 or nums[mid]> nums[mid+1])):
+                return mid
+            
+            elif (mid> low and nums[mid]< nums[mid-1]):
+                high = mid-1
+            else:
+                low = mid +1
+        return -1
+                
