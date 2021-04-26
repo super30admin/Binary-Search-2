74ecafed0aeb1dc323a4687776b7a616a79c0d34
sanjayvarma199 <sanjayvarma199@gmail.com>
2021-04-26 08:46:49 +0530

    Completed BinarySearch-2

---
+++ Find_Peak_Element.java
@@ -0,0 +1,24 @@
+class Solution {
+    //Time O(N)
+    //Space O(1)
+    public int findPeakElement(int[] nums) {
+        int low = 0 , high = nums.length-1;
+        while(low <= high)
+        {
+            int mid = low + (high-low)/2;
+            if((mid == 0 || nums[mid] > nums[mid-1]) && (mid == nums.length-1 || nums[mid] > nums[mid+1]))
+            {
+                return mid;
+            }
+            if(mid > 0 && nums[mid] < nums[mid-1])
+            {
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ Minimum_in_Rotated_Sorted_Array.java
@@ -0,0 +1,29 @@
+class Solution {
+    //Time O(logN)
+    //Space O(1)
+    public int findMin(int[] nums) {
+       int low = 0 , high = nums.length-1;
+       
+       while(low <= high)
+       {
+           if(nums[low] < nums[high])
+           {
+               return nums[low];
+           }
+           int mid = low + (high-low)/2;
+           if((mid == 0 || nums[mid] < nums[mid-1]) && (mid == nums.length-1 || nums[mid] < nums[mid+1]))
+           {
+               return nums[mid];
+           }
+           if(nums[low] <= nums[mid])
+           {
+               low = mid+1;
+           }
+           else
+           {
+               high = mid-1;
+           }
+       }
+       return -1;
+    }
+}
