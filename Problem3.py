0d564cb0b0fbf16de9d34ae5b0afd455d75413eb
manojvarmarudraraju <46211625+manojvarmarudraraju@users.noreply.github.com>
2021-05-28 20:56:42 +0530

    Completed problem 3

--- Problem3.py
+++ Problem3.py
@@ -1,6 +1,17 @@
-# Time Complexity : 
-# Space Complexity : 
-# Did this code successfully run on Leetcode : 
-# Any problem you faced while coding this : 
+# Time Complexity : O(logn)
+# Space Complexity : O(logn) for call stack
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : Took time
 
 
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        return self.binaryParse(nums,0,len(nums)-1)
+    def binaryParse(self,arr,l,r):
+        if l==r:
+            return l
+        m = (l+r)//2
+        if arr[m] > arr[m+1]:
+            return self.binaryParse(arr,l,m)
+        return self.binaryParse(arr,m+1,r)
+        
