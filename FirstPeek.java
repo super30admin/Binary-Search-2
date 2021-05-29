84ed1ec66c7a844fef6110b2c00cbb873275d4ee
Ghadi Akshay <akshay1797@gmail.com>
2021-05-28 23:12:14 -0400

    Completed Binary-Search-2

---
+++ FirstPeek.java
@@ -0,0 +1,26 @@
+// Time Complexity - O(logN)
+// Space Complexity - O(logN)
+// Worked on leetcode successfully : Yes
+
+// we use binary search to solve this problem.
+// the idea is to compare the middle element with the next element
+// if the mid is smaller than the next element then the peek is in the left subarray
+// or else its in the right subarray
+
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        
+        while (low < high) {
+            int mid = low + (high-low)/2;
+            
+            if (nums[mid] < nums[mid + 1])
+                low = mid + 1;
+            else
+                high = mid;
+        }
+        
+        return low;
+    }
+}
