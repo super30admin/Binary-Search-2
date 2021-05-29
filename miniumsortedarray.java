4d4e2bdfe389d0fb3644f64d3495649e9abb90d1
DharmeshNarola <narola.fenil@gmail.com>
2021-05-28 20:11:00 -0500

    Please BinarySearch2

--- miniumsortedarray.java
+++ miniumsortedarray.java
@@ -1,23 +1,19 @@
-public class Solution {
-    public int findMin(int[] num) {
-        if (num == null || num.length == 0) {
-            return 0;
-        }
-        if (num.length == 1) {
-            return num[0];
-        }
-        int start = 0, end = num.length - 1;
-        while (start < end) {
-            int mid = (start + end) / 2;
-            if (mid > 0 && num[mid] < num[mid - 1]) {
-                return num[mid];
-            }
-            if (num[start] <= num[mid] && num[mid] > num[end]) {
-                start = mid + 1;
+class FindMinInRotatedSortedArray {
+    public int findMin(int[] nums) {
+
+        if(nums == null || nums.length==0) return -1;
+
+        int low = 0;
+        int high = nums.length -1;
+
+        while(low < high) {
+            int mid = low+(high-low)/2;
+            if(nums[mid] > nums[high]){
+                low = mid+1;
             } else {
-                end = mid - 1;
+                high = mid;
             }
         }
-        return num[start];
+        return nums[low];
     }
-}
+} 
