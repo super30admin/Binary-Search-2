25028af36faf3949f1a6fdf307b7cd926bf4d6c8
pavankumar.karkekopp <pavan1011@gmail.com>
2021-05-28 20:49:35 -0600

    Added solution from class

---
+++ Problem1_FindTargetStartEndArray_Solution2.py
@@ -0,0 +1,86 @@
+'''
+====== Submission Details =======
+Student Name: Pavan Kumar K. N.
+S30 SlackID : RN32MAY2021
+=================================
+'''
+
+#-------------  
+# Explanation:
+#-------------
+#702. Search in a Sorted Array of Unknown Size
+# - Run two iterations of binary search. One to find start and one to find end
+
+#-----------------
+# Time Complexity: 
+#-----------------
+# O(log(N)) - 2*logN. 1st logN to find the start. 2nd to find end
+#------------------
+# Space Complexity: 
+#------------------
+# O(1) - Constant space to store indices and values
+
+
+#-----------------------
+# Leet Code Performance: 
+#-----------------------
+# Ran Successfully?: Yes
+# Time             :  84 ms (68.74 %ile)
+# Space            :  15.4 MB (47.85 %ile)
+
+from typing import List
+
+class Solution:
+
+    def binarySearchStart(self,nums, target):
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = low + ( high - low)//2
+            if nums[mid] == target:
+                if mid == 0 or nums[mid-1] < nums[mid]:
+                    return mid
+
+                high = mid-1
+
+            elif nums[mid] > target:
+                high = mid - 1
+
+            else:
+                low = mid + 1
+
+        return -1
+
+    def binarySearchEnd(self,nums, target):
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = low + ( high - low)//2
+            if nums[mid] == target:
+                if mid == len(nums) - 1 or nums[mid] < nums[mid+1]:
+                    return mid
+                low = mid+1
+
+            elif nums[mid] > target:
+                high = mid - 1
+
+            else:
+                low = mid + 1
+
+        return -1
+
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        low = 0
+        high = len(nums) - 1
+
+        start = self.binarySearchStart(nums, target)
+        if start == -1: return [-1, -1]
+
+        if nums[low] > target or nums[high] < target: 
+            return [-1, -1]
+
+        end = self.binarySearchEnd(nums, target)
+
+        return [start, end]
---
+++ Problem3_FindAnyPeakArray.py
@@ -0,0 +1,77 @@
+'''
+====== Submission Details =======
+Student Name: Pavan Kumar K. N.
+S30 SlackID : RN32MAY2021
+=================================
+'''
+
+#-------------  
+# Explanation:
+#-------------
+#162. Find Peak Element
+
+# - Idea is to use binary search type mid
+# - If nums[mid-1] <= nums[mid]  <= nums[mid+1]
+# - If nums[mid-1] > nums[mid], then there definitely is a peak on left 
+#   (high = mid-1)
+# - If nums[mid+1] < nums[mid], then there definitely is a peak on the right
+# - (low = mid + 1)
+
+#-----------------
+# Time Complexity: 
+#-----------------
+# O(log(N)) - Binary search
+#------------------
+# Space Complexity: 
+#------------------
+# O(1) - Constant space to store indices and values
+
+
+#-----------------------
+# Leet Code Performance: 
+#-----------------------
+# Ran Successfully?: Yes
+# Time             :   ( 32 ms - 99.6 %ile)
+# Space            :   ( 14.4 MB - 70.20 %ile)
+
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        low = 0
+        high = len(nums) - 1
+        
+        # We handle cases where low == high where the second element is always
+        # going to be the peak
+        while low<high:
+            mid = low + (high - low )//2
+
+            # If mid has reached left end, check if next element is < arr[mid]. 
+            if mid == 0:
+
+                #If yes, then its a peak
+                if nums[mid+1] < nums[mid]:
+                    return mid
+                
+                #Else break out of while. Array search over
+                else:
+                    break
+            
+            #If mid has reached right end check if prev element is < arr[mid]
+            elif mid == (len(nums) - 1):
+                if nums[mid-1] < nums[mid]:
+                    return mid
+                else:
+                    break
+            
+            else:
+
+                if (nums[mid-1] <= nums[mid]) and (nums[mid+1] <= nums[mid]):
+                    return mid
+               
+                elif (mid > 0) and (nums[mid-1] > nums[mid]): 
+                    high = mid - 1
+                
+                else:
+                    low = mid + 1
+
+        return high
