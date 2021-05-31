6a34e14d10651041bb3d042945f63901850ee48d
kgajwan1 <36811665+kgajwan1@users.noreply.github.com>
2021-05-31 00:20:22 -0400

    added comments

--- binary-search-2.cpp
+++ binary-search-2.cpp
@@ -1,17 +1,20 @@
+//t.c: O(logn) S.C O(1)
 class Solution {
 public:
     int findMin(vector<int> &num) {
         int start=0,end=num.size()-1;
         
         while (start<end) {
+            
+            //this means that array is not rotated
             if (num[start]<num[end])
                 return num[start];
             
             int mid = (start+end)/2;
-            
+            //pivot is in right of the mid
             if (num[mid]>=num[start]) {
                 start = mid+1;
-            } else {
+            } else {//pivot is in left of mid
                 end = mid;
             }
         }
