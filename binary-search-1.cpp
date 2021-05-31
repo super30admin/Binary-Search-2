079b2c8bfe9795c2240b71287f1fa638825a8441
kgajwan1 <36811665+kgajwan1@users.noreply.github.com>
2021-05-30 23:33:55 -0400

    added comments for context

--- binary-search-1.cpp
+++ binary-search-1.cpp
@@ -9,6 +9,10 @@ public:
         return {-1, -1};
 }
 
+ //I implemented stl function of std::lower_bound https://www.cplusplus.com/reference/algorithm/lower_bound/
+ //target+1 works because, the best pointer can go o is at the last element (notice we are returning l, not mid here) so even if we don't find target+1 in array, we simply go to whatever the next element 
+ // in the array is since function by nature has to return value not less than the target
+ //T.C O(logn), S.C O(1)
 int lower_bound(vector<int>& nums, int target) {
     int l = 0, r = nums.size()-1;
     while (l <= r) {
