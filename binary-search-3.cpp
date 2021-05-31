056ab3b386bb956873cd90422544e5aedd59a854
kgajwan1 <36811665+kgajwan1@users.noreply.github.com>
2021-05-31 00:22:48 -0400

    added comments

--- binary-search-3.cpp
+++ binary-search-3.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        
+        //O(logn) + O(1)
        int l = 0;
        int r = nums.size()-1;
-        while(l<r){
+        while(l<r){//this avoids issue with mid+1 comparison, savse me from null check
             int mid = l+(r-l)/2;
+            //if nums[mid]>nums[mid+1], we need to move left, as peak might be there, peak is guarenteed to be on side of bigger element, so it works 
             if(nums[mid]>nums[mid+1]) r = mid;
+            //else we move right
             else l = mid+1;
         }
         return l;
