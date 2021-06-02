0ef18200f3cee8b0fa5771ca0eb12e572e0ef9ea
Darpit Chaudhary <chaudhary.da@husky.neu.edu:wq>
2021-06-02 04:27:42 -0400

    Review

---
+++ FindPeakElement.java
@@ -0,0 +1,26 @@
+class FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        if(nums.length==0 || nums==null){
+            return -1;
+        }
+        if(nums.length==1){
+            return 0;
+        }
+        int high=nums.length;
+        int low=0;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if((mid==0 || nums[mid]>nums[mid-1]) &&
+               (mid==nums.length-1 || nums[mid]>nums[mid+1]))
+            {
+                return mid;
+            }
+            else if(mid!=0 && nums[mid]<nums[mid-1]){
+                high=mid-1;
+            }else{
+                low=mid+1;
+            }
+        }
+        return -1;
+    }
+}
