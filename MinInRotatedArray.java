1bd34453e890fbcebd198a339fd05568545a2fdf
Snehal Mundhe <m.snehal106@gmail.com>
2021-06-25 14:29:39 -0400

    Completed MinRotatedArray.java

--- MinInRotatedArray.java
+++ MinInRotatedArray.java
@@ -1,3 +1,23 @@
+/* Time complexity: O(log n) */
+
+/* All the test cases passed on leetcode */
+
+/* 
+Approach: 
+ 1. Using binary search algorithm, create the two pointers left - first index of the array
+ and right pointer - last index of the array, with the help of these pointers find the middle element
+ 2. If the element at middle index is greater than the element at mid + 1 index then the minimum value
+ exists at mid + 1 index, so return mid + 1
+ 3. If the element at mid index is smaller than the element at mid - 1 index then the minimum value  
+ exists at the mid index therefore return the mid index
+ 4. If none of the condition satisfied then check if the mid element is greater the element at index 0, 
+ then that means the minimum element exists on the right side of the mid index, if this condition does not
+ satisfy that means the minimum element exists at the left side of the mid element
+ 5. Repeat this process until one of the condition is satisfied or when left and right pointers 
+ are pointing to the same element
+
+*/
+
 class MinInRotatedArray {
     public int findMin(int[] nums) {
         int n = nums.length;
