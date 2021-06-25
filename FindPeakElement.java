448be6195ffceeb2c9ae5e02cab80b3460bee672
Snehal Mundhe <m.snehal106@gmail.com>
2021-06-25 14:07:06 -0400

    Completed FindPeakElement.java

--- FindPeakElement.java
+++ FindPeakElement.java
@@ -1,3 +1,20 @@
+/* Time complexity: O(log n) */
+
+/* All test cases passed on leetcode */
+
+/* Appraoch:
+  1. Using binary search, the array is divided to the point at which only
+  single element remains in the array that is the peak element.
+  2. Find the mid index element, compare the mid element with the element at mid + 1,
+  if the element at mid is greater than the element at mid + 1, then the peak exists 
+  on the left side of the mid and thus, the right pointer will be updated to mid
+  else if the element at mid is smaller than the element at mid + 1 then the peak exists
+  at the right of the mid element therefore the left pointer is updated to mid + 1
+  3. Repeat this process until only one element is left and that element would be the peak element
+*/
+
+
+
 class findPeakElement {
     public int findPeakElement(int[] nums) {
         int n = nums.length;
