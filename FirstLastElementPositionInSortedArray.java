a27f5ca9c77b0b4e02c430097ddfee30134ad320
jisakp7 <kavya.pathi@jbhunt.com>
2021-06-26 02:43:03 -0500

    FirstLastElementPositionInSortedArray.java

--- FirstLastElementPositionInSortedArray.java
+++ FirstLastElementPositionInSortedArray.java
@@ -9,47 +9,40 @@
  */
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if(nums==null) return null;
-        int start=0, end = nums.length-1;
-        if(nums.length==1 ){
-            if(nums[0]==target){
-                return new int[]{0,0};
-            } else {
-                return new int[]{-1,-1};
-            }
-        }
-        return new int[]{findFirst(nums, target),findLast(nums, target)};
+        if(nums==null || nums.length==0) return new int[]{-1, -1};
+
+        return new int[]{binarySearch(nums, target, true), binarySearch(nums, target, false)};
     }
 
-    private int findFirst(int[] nums, int target){
-        int idx = -1;
-        int start = 0;
-        int end = nums.length - 1;
-        while(start <= end){
-            int mid = start + (end - start) / 2;
-            if(nums[mid] >= target){
-                end = mid - 1;
-            }else{
-                start = mid + 1;
-            }
-            if(nums[mid] == target) idx = mid;
-        }
-        return idx;
-    }
+    private int binarySearch(int[] nums, int target, boolean isFirst){
+        int start=0;
+        int end = nums.length-1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid]==target){
+                if(isFirst){
+                    if(mid==0 || nums[mid-1]!=target) return mid;
+                    else
+                        start=mid-1;
+                    end=mid-1;
 
-    private int findLast(int[] nums, int target){
-        int idx = -1;
-        int start = 0;
-        int end = nums.length - 1;
-        while(start <= end){
-            int mid = start +(end -start) / 2;
-            if(nums[mid] <= target){
-                start = mid + 1;
-            }else{
-                end = mid - 1;
+                } else{
+                    if(mid==nums.length-1 || nums[mid+1]!=target) return mid;
+                    else{
+                        start = mid+1;
+                        end = mid+1;
+                    }
+                }
             }
-            if(nums[mid] == target) idx = mid;
+
+            else if(nums[mid]>target){
+                end = mid-1;
+            }
+            else{
+                start= mid+1;
+            }
+
         }
-        return idx;
+        return -1;
     }
 }
