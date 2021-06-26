784154a232ec48db67c1811e589b8d52c96d5db7
jisakp7 <kavya.pathi@jbhunt.com>
2021-06-26 03:35:38 -0500

    FindPeekElement.java

--- FindPeekElement.java
+++ FindPeekElement.java
@@ -1,3 +1,13 @@
+//Problem
+162. Find Peak Element
+        A peak element is an element that is strictly greater than its neighbors.
+        Given an integer array nums,find a peak element,and return its index.If the array contains multiple peaks,return the index to any of the peaks.
+
+        You may imagine that nums[-1]=nums[n]=-∞.
+
+        You must write an algorithm that runs in O(log n)time.
+
+
 // Time Complexity : O(logN)
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
@@ -10,21 +20,19 @@ peak element would be in left variable;
  */
 class Solution {
     public int findPeakElement(int[] nums) {
-        int left=0;
-        int right=nums.length-1;
+        if (nums == null || nums.length == 0) return -1;
+        int start = 0, end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if ((mid == 0 || nums[mid] > nums[mid - 1]) && (mid == nums.length - 1 || nums[mid] > nums[mid + 1]))
+                return mid;
+            else if (nums[mid] < nums[mid + 1]) {
+                start = mid + 1;
+            } else
+                end = mid - 1;
 
-        while(left<right){
-
-            int mid=left+(right-left)/2;
-            if(nums[mid]>nums[mid+1]){
-                right=mid;
-
-            }
-            else {
-                left=mid+1;
-            }
         }
 
-        return left;
+        return -1;
     }
 }
--- FirstLastElementPositionInSortedArray.java
+++ FirstLastElementPositionInSortedArray.java
@@ -1,3 +1,14 @@
+//Problem
+
+34. Find First and Last Position of Element in Sorted Array
+
+        Given an array of integers nums sorted in ascending order,find the starting and ending position of a given target value.
+
+        If target is not found in the array,return[-1,-1].
+
+        You must write an algorithm with O(log n)runtime complexity.
+
+
 // Time Complexity : O(logN)
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
@@ -9,37 +20,34 @@
  */
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        if(nums==null || nums.length==0) return new int[]{-1, -1};
+        if (nums == null || nums.length == 0) return new int[]{-1, -1};
 
         return new int[]{binarySearch(nums, target, true), binarySearch(nums, target, false)};
     }
 
-    private int binarySearch(int[] nums, int target, boolean isFirst){
-        int start=0;
-        int end = nums.length-1;
-        while(start<=end){
-            int mid = start + (end-start)/2;
-            if(nums[mid]==target){
-                if(isFirst){
-                    if(mid==0 || nums[mid-1]!=target) return mid;
+    private int binarySearch(int[] nums, int target, boolean isFirst) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                if (isFirst) {
+                    if (mid == 0 || nums[mid - 1] != target) return mid;
                     else
-                        start=mid-1;
-                    end=mid-1;
+                        start = mid - 1;
+                    end = mid - 1;
 
-                } else{
-                    if(mid==nums.length-1 || nums[mid+1]!=target) return mid;
-                    else{
-                        start = mid+1;
-                        end = mid+1;
+                } else {
+                    if (mid == nums.length - 1 || nums[mid + 1] != target) return mid;
+                    else {
+                        start = mid + 1;
+                        end = mid + 1;
                     }
                 }
-            }
-
-            else if(nums[mid]>target){
-                end = mid-1;
-            }
-            else{
-                start= mid+1;
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
             }
 
         }
