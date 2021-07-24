a456d34f5291314783cfb283f8906b166444c52e
Lokesh Ramdin <lokeshramdin@Lokeshs-MacBook-Pro.local>
2021-07-24 18:11:38 -0500

    Completed problem2.java from Binary-search-2.

---
+++ Problem2.java
@@ -0,0 +1,30 @@
+class Problem2 {
+    public int findMin(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return -1;
+        }
+        int low = 0, high = nums.length -1;
+        
+        //condition for unrotated sorted array. example: num = [1,2,4]
+        if(nums[low] <= nums[high]) {
+            return nums[low];
+        }
+        
+        while(low <= high) {
+            int mid = low + (high -low)/2;
+            if(nums[low] <= nums[high]){
+                return nums[low];
+            }
+            //base case
+            if((mid ==0 || nums[mid] < nums[mid-1]) && (mid == nums.length -1 ||nums[mid] < nums[mid +1])) {
+                return nums[mid];
+            }
+            else if(nums[low] <= nums[mid]) {
+                low = mid +1;
+            } else{
+                high = mid -1;
+            }
+        }
+        return 40584;
+    }
+}
