7f2a005e8d2305518cf92ddc533b2013b0d0d804
Srushti Yadhunath <yadhunat@usc.edu>
2021-08-05 14:50:58 -0700

    Completed Binary Search 2 please review

---
+++ Problem1.java
@@ -0,0 +1,68 @@
+// Time Complexity : O(log n) 
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding: No
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        if (nums == null || nums.length ==0){
+            return new int[]{-1, -1};
+        }
+        
+        //int low = 0; 
+        //int high = nums.length - 1; 
+        int left = binarySearchLeft(nums, target);
+        if (left == -1){
+            return new int[] {-1,-1};
+        }
+        int right = binarySearchRight(nums,target);
+        return new int[] {left,right};
+    }
+    
+    private int binarySearchLeft(int[] nums, int target){
+        int low = 0;
+        int high = nums.length -1; 
+        while (low<=high){
+            int mid = low +(high-low)/2;
+            if (nums[mid]==target){
+                if (mid==0 || mid==nums.length-1 || nums[mid-1]<target){
+                    return mid;
+                }
+                else if (nums[mid-1]== target) {
+                    high = mid-1;
+                }
+            }
+            else if (target>nums[mid]){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+    private int binarySearchRight(int[] nums, int target){
+        int low =0;
+        int high = nums.length -1;
+        while (low<=high){
+            int mid = low + (high-low)/2;
+            if (nums[mid]==target){
+                if (mid == nums.length-1 ||nums[mid+1]>target){
+                    return mid;
+                }
+                else if (nums[mid+1]==target){
+                    low = mid+1;
+                }
+            }
+            else if (target<nums[mid]){
+                high= mid-1;
+            }
+            else {
+                low = mid+1;
+            }
+            
+        }
+        return -1;
+    }
+}
+    
