5e78174100458ab63fd503c0824a0f746c03d6cc
Abhirag Nagpure <nagpureabhirag@gmail.com>
2021-08-19 23:46:44 -0500

    Completed Sprint Binary-Search-2: Please Review

---
+++ peak_element.py
@@ -0,0 +1,12 @@
+# Did this code successfully run on Leetcode : YES
+
+class Solution:
+	def findPeakElement(self, nums: List[int]) -> int:
+		l, r = 0, len(nums)-1
+		while l < r:
+			m = (l+r)//2
+			if nums[m] > nums[m+1]:
+				r = m
+			else:
+				l = m + 1
+		return l
