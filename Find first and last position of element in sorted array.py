ba2d16ea734fd75ea5e72d76e6ed6ca315c7bef1
Subhash <svuppal6@asu.edu>
2021-09-23 16:52:16 -0700

    Please review

--- Find first and last position of element in sorted array.py
+++ Find first and last position of element in sorted array.py
@@ -1,15 +1,40 @@
-# SC- O(n)
+# SC- O(logn)
 # TC-O(1)
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        count = 0
-        start_num = last_num = -1
-        for i in range(len(nums)):
-            if nums[i] == target:
-                count += 1
-                if count == 1:
-                    start_num = i
-                    last_num = i
-                else:
-                    last_num = i
-        return [start_num, last_num]
+        startpoint = self.start_point(nums, target)
+        if startpoint == -1:
+            return [-1, -1]
+        endpoint = self.end_point(nums, target)
+        return [startpoint, endpoint]
+
+    def start_point(self, nums: List[int], target: int):
+        low = 0
+        high = len(nums) - 1
+        while (low <= high):
+            mid = low + (high - low) // 2
+            if (nums[mid] == target):
+                if (mid == 0 or nums[mid - 1] != target):
+                    return mid
+                high = mid - 1
+            elif (nums[mid] > target):
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
+
+    def end_point(self, nums: List[int], target: int):
+        low = 0
+        high = len(nums) - 1
+        while (low <= high):
+            mid = low + (high - low) // 2
+            if (nums[mid] == target):
+                if (mid == len(nums) - 1 or nums[mid + 1] != target):
+                    return mid
+                low = mid + 1
+            elif (nums[mid] > target):
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
+
