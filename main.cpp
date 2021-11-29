66e0d2dd9c855a078b98656e6b3da1eb723278a9
SaiGowtham-11 <61152047+SaiGowtham-11@users.noreply.github.com>
2021-11-28 19:26:01 -0800

    Find Minimum in Rotated Sorted Array

--- main.cpp
+++ main.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
-    int findPeakElement(vector<int>& nums) {
-        int low = 0;
-        int high = nums.size()-1;
-        int mid = 0;
+    int findMin(vector<int>& nums){
+        int low  = 0;//0
+        int high = nums.size()-1;//4
+        int mid = 0;//2
 
         while(low <= high){
             mid = low + (high - low)/2;
-            if((mid == 0 || nums[mid] > nums[mid-1]) && (mid == nums.size()-1 || nums[mid] > nums[mid+1]))             {
-                return mid;
+            if(nums[low] <= nums[high]){
+                return nums[low];
             }
-            else if(nums[mid] < nums[mid+1]){
+            else if((mid == 0 || nums[mid] < nums[mid-1]) && (mid == nums.size()-1 ||nums[mid] < nums[mid+1])){
+                return nums[mid];
+            }else if(nums[low] <= nums[mid]){
                 low = mid+1;
-            } else{
-                high = mid-1;
+            }else {
+                high = mid -1;
             }
         }
-        return low;
+        return -1;
     }
 };
