d887080d60240ceb0e380f60949ee1c6b7946e9a
Dhruv Jani <dhruv.jani@rocketmail.com>
2021-12-25 18:41:49 -0500

    Completed Binary Search 2 Please Review

---
+++ FirstLastElement.java
@@ -0,0 +1,70 @@
+// Time Complexity :O(logn)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach
+
+class FirstLastElement {
+    public int[] searchRange(int[] nums, int target) {
+        if(nums == null || nums.length == 0){
+            return new int[]{-1,-1};
+        }
+        if(nums[0] == target && nums[nums.length -1] == target){
+            return new int[] {0, nums.length -1};
+        }
+        int left = binarySearchLeft(nums, target);
+        if (left == -1){
+            return new int[]{-1,-1};
+        }
+        int right = binarySearchRight(nums,target, left);
+        
+        return new int[] {left, right};
+        
+        
+    }
+    
+    private int binarySearchLeft(int[] nums, int target){
+        int low = 0, high = nums.length -1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+                if(mid == 0 || nums[mid - 1] < nums[mid]){
+                    return mid;
+                }
+                else{
+                    high = mid -1;
+                }
+            }
+            else if(target < nums[mid]){
+                high = mid -1; 
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+    private int binarySearchRight(int[] nums, int target, int low){
+        low = low; int  high = nums.length -1;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(nums[mid] == target){
+               if(mid == nums.length -1 || nums[mid + 1] > nums[mid]){
+                   return mid;
+               }
+                else{
+                    low = mid + 1;
+                }
+            }
+            else if(target < nums[mid]){
+                high = mid -1; 
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
---
+++ PeakElement.java
@@ -0,0 +1,23 @@
+// Time Complexity :O(logn)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :
+
+class PeakElement {
+    public int findPeakElement(int[] nums) {
+        if(nums == null || nums.length == 0) return -1; 
+        int low = 0, high = nums.length -1; 
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if((mid == 0 || nums[mid] > nums[mid -1]) && (mid == nums.length -1 || nums[mid] > nums[mid + 1])){
+                return mid;
+            }
+            else if(nums[mid + 1] > nums[mid]){
+                low = mid + 1;
+            }else{
+                high = mid -1; 
+            }
+        }
+        return 5321321; 
+    }
+}
