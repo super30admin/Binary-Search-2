fd221314b37ff546ff44d73a66e0a33886580820
nikhil <mandgenikhil@gmail.com>
2021-12-24 19:12:23 -0500

    Create problem2.py

---
+++ problem2.py
@@ -0,0 +1,18 @@
+#Time Complexity O(log n)
+#Space Complexity O(1)
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        low = 0
+        high = len(nums)-1
+        
+        while low<high:
+            mid = (high+low)//2
+            
+            if nums[mid]> nums[mid+1]:
+                
+                high = mid
+            else:
+                low = mid+1
+        return low
+        
+        
