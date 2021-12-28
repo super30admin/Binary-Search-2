7ea6a10b11a16b783ddbe3ca1b8f615fc4fa9b50
Prateek Sarna <prateeksarna@Prateeks-MacBook-Pro.local>
2021-12-27 16:45:10 -0800

    Completed Sprint BinarySearch2

---
+++ src/FindPeakElement.java
@@ -0,0 +1,24 @@
+// Time Complexity :O(log(n))
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+
+class FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if ((mid == 0 || nums[mid] > nums[mid - 1]) && (mid == nums.length - 1 || nums[mid] > nums[mid + 1]))
+                return mid;
+            else {
+                if (mid != nums.length - 1  && nums[mid] < nums[mid + 1])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
