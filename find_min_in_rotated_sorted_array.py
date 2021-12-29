a2bf72dd6b9cbf6be3e59bf6c707610889b93ec1
ravirammamidi <tatsuya.s.mknr@gmail.com>
2021-12-28 19:10:39 -0700

    solutions after class discussion

--- find_min_in_rotated_sorted_array.py
+++ find_min_in_rotated_sorted_array.py
@@ -1,9 +1,6 @@
+from typing import List
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
         if len(nums) == 1:return nums[0]
         l, r = 0 , len(nums) -1
         while l<=r:
