ccc16815d6549a95025700ea843d894000c9c207
Jayavardhan Karampudi <jay@Jais-MacBook-Pro.local>
2021-12-29 02:02:59 -0700

    solved the problems

---
+++ find_min_in_rotated_sorted_array.py
@@ -0,0 +1,19 @@
+# avg TimeComplexity O(log n )
+# SpaceComplexity O(1)
+
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0: return -1
+        low, high = 0, len(nums)-1
+        while low <= high:
+            if nums[low] < nums[high]: return nums[low]
+            mid = (low + high)//2
+            if (mid == 0 or nums[mid] < nums[mid-1]) and (mid == len(nums)-1 or nums[mid] < nums[mid+1]): return nums[mid]
+            elif nums[low] <= nums[mid]:
+                low = mid + 1
+            else:
+                high = mid - 1
