cff7f059561b0769857fb9eeb1fc48d9f868aa2e
PRAYAG-PATEL-1998 <ppatel94@asu.edu>
2022-01-03 17:28:24 -0700

    Completed Sprint Binary Search 2 Problem 9,8,10

---
+++ p_10.java
@@ -0,0 +1,28 @@
+// Time Complexity :O(log(n))
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :yes
+// Any problem you faced while coding this :no
+
+
+// Your code here along with comments explaining your approach
+class Solution{
+    public int findPeakElement(int [] nums){
+        if(nums == null || nums.length == 0){
+            return -1;
+        }
+        int low = 0;
+        int high = nums.length - 1 ;
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            //if mid is peak
+            if((mid == 0 || nums[mid] > nums[mid - 1]) && (mid == nums.length - 1 || nums[mid] > nums[mid + 1])){
+                return mid;
+            }else if(mid != 0 && nums[mid] < nums[mid - 1]){
+                high = mid - 1;
+            }else{
+                low = mid + 1;  
+            }
+        }
+        return 99;
+    }
+}
