df4a6cbe2e9b84f2c16036e49ed0581de0efb3bf
gunjalgupta <78285301+gunjalgupta@users.noreply.github.com>
2022-01-13 07:16:51 -0800

    Create FirstLast.java

---
+++ FirstLast.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int start=-1; int end = -1;
+      
+        
+        for (int i=0; i< nums.length ; i++){
+            if(nums[i]== target) {
+                start = i;
+                end=i;
+                break;
+            }
+        }
+        if (start == -1) return new int[]{-1,-1};
+        for (int i=nums.length-1; i>start ; i--){
+            if(nums[i]== target) {
+                end = i;
+                break;
+            }
+        }
+        return new int[]{start,end};
+    }
+}
