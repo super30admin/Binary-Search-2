068dde04965dd03378577c1da9f1c2bd7686c226
SRaval20 <raval.sagar.sr@gmail.com>
2022-02-04 23:07:04 -0800

    Completed Binary-Search-2

--- src/firstLastPosInSorted.java
+++ src/firstLastPosInSorted.java
@@ -1,2 +1,73 @@
-package src;public class firstLastPosInSorted {
+// Operation:               search
+// Time Complexity:       O(logm*logn)
+// Space Complexity:         O(1)
+// Yes, this code ran successfully
+// No, I didn't face any problem in this problem statement after understanding the logic in class
+
+
+package src;
+
+public class firstLastPosInSorted
+{
+    public static void main(String[] args) {
+        int[] arr = new int[] {5,7,7,8,8,10,11} ;
+        int target = 8 ;
+        Solution s = new Solution() ;
+        int[] ans = s.searchRange(arr, target) ;
+        System.out.println("First Pos:"+ans[0]+", Last Pos: "+ans[1]) ;
+    }
+}
+
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int l = 0 ;
+        int r = nums.length - 1 ;
+
+        int start = searchStart(nums, l, r, target) ;                       // finding start position of number in array
+        int end = searchEnd(nums, l, r, target, start) ;                    // finding end position of number in array
+        int[] pos = new int[] {start, end} ;
+        return pos ;
+    }
+
+    private int searchStart(int[] nums, int l, int r, int target)
+    {
+        while(l<=r)
+        {
+            int m = l + (r-l)/2 ;
+            if(nums[m] == target)
+            {
+                if(m==0 || nums[m-1]!=target)                               // if first element is num OR element before mid element is not num
+                    return m ;
+                else                                                        // There is at least one element before mid which is num
+                    r = m-1 ;
+            }
+            else if(nums[m] < target)                                       // haven't reached target yet
+                l = m + 1 ;
+            else                                                            // haven't reached target yet
+                r = m - 1 ;
+        }
+        return -1 ;                                                         // num not present
+    }
+
+    private int searchEnd(int[] nums, int l, int r, int target, int start)
+    {
+        if(start == -1)
+            return -1 ;
+        while(l<=r)
+        {
+            int m = l + (r-l)/2 ;
+            if(nums[m] == target)
+            {
+                if(m==nums.length-1 || nums[m+1]!=target)                   // if last element is num OR element after mid element is not num
+                    return m ;
+                else                                                        // There is at least one element after mid which is num
+                    l = m+1 ;
+            }
+            else if(nums[m] > target)                                       // haven't reached target yet
+                r = m - 1 ;
+            else                                                            // haven't reached target yet
+                l = m + 1 ;
+        }
+        return -1 ;
+    }
 }
--- src/minRotated.java
+++ src/minRotated.java
@@ -1,2 +1,46 @@
-package src;public class minRotated {
+// Operation:              search
+// Time Complexity:       O(logn)
+// Space Complexity:        O(1)
+// Yes, this code ran successfully
+// No, I didn't face any problem in this problem statement
+
+
+package src;
+
+public class minRotated
+{
+    public static void main(String[] args)
+    {
+        int[] arr = new int[]{4,5,1,2,3} ;
+        Min s = new Min() ;
+        System.out.println("Min val: "+s.findMin(arr)) ;
+    }
+}
+
+class Min {
+    public int findMin(int[] nums)
+    {
+        int l = 0 ;
+        int r = nums.length - 1 ;
+        int mid = 0 ;
+        while(l < r)
+        {
+            mid = l + (r-l)/2 ;                                             // getting mid index
+            if(nums[l]>nums[mid])                                           // if left sub-array is unsorted
+            {
+                if(nums[mid-1]>nums[mid])                                   // checking if num at mid is minimum or not
+                    return nums[mid] ;                                      // if yes, return minimum value
+                r = mid - 1 ;                                               // else, continue to left sub-array
+            }
+            else if(nums[mid]>nums[r])                                      // if right sub-array is unsorted
+            {
+                if(nums[mid]>nums[mid+1])                                   // checking if num at mid is minimum or not
+                    return nums[mid+1] ;                                    // if yes, return minimum value
+                l = mid + 1 ;                                               // else, continue to right sub-array
+            }
+            else
+                return nums[l] ;                                            // if array is already sorted
+        }
+        return nums[mid] ;                                                  // returning value at mid index
+    }
 }
