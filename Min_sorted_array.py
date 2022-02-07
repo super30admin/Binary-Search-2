54ee0059183af8a35c012d81a7e7ccaa092b6965
Mounica5K <98117848+Mounica5K@users.noreply.github.com>
2022-02-06 19:09:59 -0800

    Optimized Min finding in sorted array

--- Min_sorted_array.py
+++ Min_sorted_array.py
@@ -9,11 +9,15 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         s, e = 0, len(nums)-1
-        while(s<e):
-            mid = (s+e)//2
-            if nums[mid] <= nums[e]:
-                e = mid
+        if nums[s] < nums[e] or len(nums)<2:
+            return nums[s] # array is sorted already
+        while(s<=e):
+            mid = (e-s)//2+s
+            #finding pivot element
+            if mid != 0 and nums[mid-1]>nums[mid]:
+                return nums[mid]
+            elif nums[mid] < nums[e]:
+                e = mid -1 
             else:
                 s = mid +1
-        return nums[s]
         
