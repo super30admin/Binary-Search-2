8a89eb62035e1b20a612ca95c901b004df4d4893
Ravi Teja Natchireddi <daredevilteja@outlook.com>
2022-03-05 16:00:31 +0000

    formatted code

--- FindPeakElement.java
+++ FindPeakElement.java
@@ -8,9 +8,9 @@
 class Solution {
     public int findPeakElement(int[] nums) {
         int low = 0, high = nums.length - 1, mid;
-        while(high >= low) {
-            mid = low + (high - low)/2;
-            if(mid + 1 < nums.length && (nums[mid+1] > nums[mid])) {
+        while (high >= low) {
+            mid = low + (high - low) / 2;
+            if (mid + 1 < nums.length && (nums[mid + 1] > nums[mid])) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
