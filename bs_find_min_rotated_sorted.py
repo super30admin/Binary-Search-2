41ff7905bbab3827c5c717d087cffb865e150d53
knatarasan <kannappan_natarasan@yahoo.com>
2022-03-05 09:40:41 -0800

    Found rotate index and returned the element at the rotate index

--- bs_find_min_rotated_sorted.py
+++ bs_find_min_rotated_sorted.py
@@ -11,7 +11,7 @@
     - else
         - discard left array if it was sorted [ mid is greater than left]
         - discard right array if it was sorted 
-2. Based on rotateIndex , search for target in either left or right sub array
+2. Return the element from rotateIndex 
 '''
 
 
@@ -22,37 +22,23 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        # If the list has just one element then return that element.
-        if len(nums) == 1:
+        if len(nums)==1:
             return nums[0]
 
-        # left pointer
-        left = 0
-        # right pointer
-        right = len(nums) - 1
+        rotate_idx=self.find_rotate_index(nums,0,len(nums)-1)
+        return nums[rotate_idx]
+        
+        
+    def find_rotate_index(self,nums,left, right):
+        if nums[left] < nums[right]:
+            return 0
 
-        # if the last element is greater than the first element then there is no rotation.
-        # e.g. 1 < 2 < 3 < 4 < 5 < 7. Already sorted array.
-        # Hence the smallest element is first element. A[0]
-        if nums[right] > nums[0]:
-            return nums[0]
-
-        # Binary search way
-        while right >= left:
-            # Find the mid element
-            mid = left + (right - left) // 2
-            # if the mid element is greater than its next element then mid+1 element is the smallest
-            # This point would be the point of change. From higher to lower value.
-            if nums[mid] > nums[mid + 1]:
-                return nums[mid + 1]
-            # if the mid element is lesser than its previous element then mid element is the smallest
-            if nums[mid - 1] > nums[mid]:
-                return nums[mid]
-
-            # if the mid elements value is greater than the 0th element this means
-            # the least value is still somewhere to the right as we are still dealing with elements greater than nums[0]
-            if nums[mid] > nums[0]:
-                left = mid + 1
-            # if nums[0] is greater than the mid value then this means the smallest value is somewhere to the left
+        while left <= right:
+            pivot = (left + right) // 2
+            if nums[pivot] > nums[pivot + 1]:
+                return pivot + 1
             else:
-                right = mid - 1
+                if nums[pivot] < nums[left]:
+                    right = pivot - 1
+                else:
+                    left = pivot + 1
