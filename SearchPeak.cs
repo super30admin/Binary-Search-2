4924e6aad012babd5be247b54148be0252d0fb9a
nilesh-ghodke <99153455+nilesh-ghodke@users.noreply.github.com>
2022-04-02 17:22:04 -0700

    Add files via upload

---
+++ SearchPeak.cs
@@ -0,0 +1,36 @@
+﻿namespace ConsoleApp1
+{
+
+    public class FindPeak
+    {
+        public int FindPeakElement(int[] nums)
+        {
+            int low = 0;
+            int high = nums.Length - 1;
+
+            while (low <= high)
+            {
+                int mid = low + (high - low) / 2;
+
+
+                if((mid==nums.Length-1 ||nums[mid]>nums[mid+1]) &&
+                    (mid==0 || nums[mid]>nums[mid-1]))
+                {
+                    return mid;
+                }
+
+                if (mid !=0 && (nums[mid-1] > nums[mid]))
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+
+            }
+
+            return 456763;
+        }
+    }
+}
