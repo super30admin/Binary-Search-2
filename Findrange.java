4c537feb03bedbc3dbf12434c3dd3a7f9b00b525
Pratik Kamble <54627841+pratik-kamble@users.noreply.github.com>
2022-04-05 18:45:22 -0400

    Corrected

--- Findrange.java
+++ Findrange.java
@@ -3,70 +3,74 @@
 //Solved on Leet code : No ( Fails for [2,2] 3)
 // Approach : binary search twice
 class Findrange {
+   class Solution {
     public int[] searchRange(int[] nums, int target) {
-        
+       
         if(nums== null || nums.length == 0){
-            return new int[] {-1,-1};
+            return new int[]{-1,-1};
         }
-        int firstIndex = searchFirstElement(nums,target);
-        if(firstIndex == -1){
-             return new int[] {-1,-1};
+        if(target< nums[0] || target > nums[nums.length-1]){
+             return new int[]{-1,-1};
         }
-        int secondIndex = searchSecondElement(nums,target);
-        
-        int[] result =  {firstIndex,secondIndex};
-        return result; 
-        
+        int first,last;
+        first = findFirstIndex(nums, target);
+        if(first == -1){
+            return new int[]{-1,-1};
+        }
+        last = findSecondIndex(nums, target);
+        return new int[]{first,last};
     }
     
-    private int searchFirstElement(int[] nums, int target){
-        int low = 0;
-        int high = nums.length;
-        int mid;
-        
-        while(low <= high){
-            mid = low + (high-low)/2;
-            
-            if(nums[mid] == target){
-                if(mid == 0 || nums[mid-1] < target){
-                    return mid;
-                }else{
-                    high = mid -1;
-                }   
-            }
-            if(target < nums[mid]){
-                high = mid - 1;
-            }else{
-                low = mid + 1;
-            } 
-        }
-        return -1;
-    }
-    
-     private int searchSecondElement(int[] nums, int target){
+    private int findFirstIndex(int[] nums, int target){
         int low = 0;
         int high = nums.length - 1;
-        int mid;
-        while(low <= high){
-            mid = low + (high - low)/2;
+        
+        while(low<=high){
+            
+            int mid = low + (high-low)/2;
             
             if(nums[mid] == target){
-                if(mid == nums.length-1 || nums[mid+1] > target){
-                    return mid;
-                }else{
-                    low = mid+1;
-                }
-            }
-            if(target < nums[mid]){
+               if (mid == 0 || nums[mid-1]<nums[mid]){
+                return mid;
+            }else {
                 high = mid -1;
-            }
-            else{
+            } 
+            } 
+            else if(target > nums[mid]){
                 low = mid + 1;
+            }else{
+                high = mid - 1;
             }
             
         }
+      return -1;  
+    }
+    
+    private int findSecondIndex(int nums[], int target){
+        int low = 0;
+        int high = nums.length -1;
+        
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            
+            if(nums[mid] == target){
+               if (mid == nums.length-1 ||nums[mid+1] > nums[mid]){
+                return mid;
+                }
+                else{
+                low = mid + 1;
+                } 
+            } 
+            else if(target >nums[mid]){
+                low = mid +1;
+            }else {
+                high = mid-1;
+            }
+        }
         return -1;
     }
+    
+}
     
     
 }
