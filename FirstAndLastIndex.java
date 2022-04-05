83fd7bad6ec9c44bcd7ddbfff469285b5b19bdd0
eshasah <esha8sah@gmail.com>
2022-04-05 00:01:49 -0700

    Solution

--- FirstAndLastIndex.java
+++ FirstAndLastIndex.java
@@ -9,7 +9,19 @@ class FirstAndLast{
     public int[] searchRange(int[] nums, int target) {
         int[] res = new int[]{-1,-1};
         if(nums == null || nums.length == 0) return res;
+
+        if(target < nums[0] || target > nums[nums.length-1]) return res;
         
+        int first = binarySearchFirst(nums, target);
+        if(first == -1){
+            return res;
+        }
+
+        int second = binarySearchSecond(nums, target);
+        return new int[] {first, second};
+    }
+    
+    public int binarySearchFirst(int[] nums, int target){
         int low = 0;
         int high = nums.length-1;
         
@@ -17,9 +29,12 @@ class FirstAndLast{
             int mid = low + (high-low)/2;
             
             if(nums[mid] == target){
-                res[0] = firstOccurance(nums, target, mid);
-                res[1] = lastOccurance(nums, target, mid);
-                break;
+                if(mid == 0 || nums[mid-1] < nums[mid]){
+                    return mid;
+                }
+                else{
+                    high = mid-1;
+                }
             }
             else if(target < nums[mid]){
                 high = mid-1;
@@ -28,22 +43,31 @@ class FirstAndLast{
                 low = mid+1;
             }
         }
-        return res;
+        return -1;
     }
     
-    public int firstOccurance(int[] nums, int target, int mid){
-        int idx = mid;
-        while(idx-1 >= 0 && nums[idx-1] == target){
-            idx--;
+    public int binarySearchSecond(int[] nums, int target){
+        int low = 0;
+        int high = nums.length-1;
+        
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            
+            if(nums[mid] == target){
+                if(mid == nums.length-1 || nums[mid+1] > nums[mid]){
+                    return mid;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            else if(target < nums[mid]){
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
         }
-        return idx;
-    }
-    
-    public int lastOccurance(int[] nums, int target, int mid){
-        int idx = mid;
-        while(idx+1 < nums.length && nums[idx+1] == target){
-            idx++;
-        }
-        return idx;
+        return -1;
     }
 }
