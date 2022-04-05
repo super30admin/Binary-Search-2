f1c0f8f4b097f77acddcf4caa7b757f8e0388a8f
manish824 <manishsaitalasila2000@gmail.com>
2022-04-05 14:41:24 +0530

    updated

--- simply.py
+++ simply.py
@@ -1,11 +1,18 @@
 class Solution:
-    def findPeakElement(self, nums: List[int]) -> int:
+    def findMin(self, nums: List[int]) -> int:
         l=0
         r=len(nums)-1
-        while(l<r):
-            m=(l+r)//2
-            if(nums[m]>nums[m+1]):
-                r=m
+        while(l<=r):
+            if(nums[l]<=nums[r]):
+                return nums[l]
+            if(r==1):
+                return min(nums)
+            mid=(l+r)//2
+            
+            if(mid==0 or nums[mid]<nums[mid-1] or mid==len(nums)-1 or nums[mid]==nums[mid+1]):
+                return nums[mid]
+            elif nums[l]<=nums[mid] :
+                l=mid+1
             else:
-                l=m+1
-        return l
+                r=mid-1
+        return -1     
