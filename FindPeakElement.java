bf6d6418d2bd58141b7d91a35c3c54a57558bf11
arshmohd <irfan.arshad4@gmail.com>
2022-04-29 18:22:25 +0530

    Implemented the algo for FindPeakElement in logn time complexity and O(1) space complexity

--- FindPeakElement.java
+++ FindPeakElement.java
@@ -1,5 +1,28 @@
 public class FindPeakElement {
-  public int findPeakElement(int[] nums) {
 
+  public int findPeakElement(int[] nums) {
+    int leftIdx = 0, rightIdx = nums.length - 1, midIdx;
+    while (leftIdx <= rightIdx) {
+      midIdx = leftIdx + (rightIdx - leftIdx) / 2;
+      if ((midIdx == nums.length - 1 || nums[midIdx] > nums[midIdx + 1])
+          && (midIdx == 0 || nums[midIdx] > nums[midIdx - 1])) {
+        return midIdx;
+      }
+
+      else if (midIdx != 0 && nums[midIdx] < nums[midIdx - 1]) {
+        rightIdx = midIdx - 1;
+      } else {
+        leftIdx = midIdx + 1;
+      }
+
+    }
+    return -1;
+  }
+
+  public static void main(String args[]) {
+    FindPeakElement fPE = new FindPeakElement();
+    int[] nums = { 1, 2, 3, 1 };
+    int result = fPE.findPeakElement(nums);
+    System.out.println(result);
   }
 }
