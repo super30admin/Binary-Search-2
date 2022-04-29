da9f3ad641ac3b44bd2f7a9aa63bd7edc33030d5
Rutuja Mandlik <mandlik.r@northeastern.edu>
2022-04-29 19:48:42 -0400

    2 problems completed

--- findFirstLastPositionSortedArrBST.java
+++ findFirstLastPositionSortedArrBST.java
@@ -1,77 +1,56 @@
-// Time Complexity : O(log n)
-// Space Complexity : O(1)
-// Did this code successfully run on Leetcode : Yes
-// Any problem you faced while coding this : No
-
-
-// Your code here along with comments explaining your approach
+//TC: O(log n)
+//SC: O(1)
 
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        
-        //null check
-        if(nums == null || nums.length == 0) return new int[]{-1,-1};
-        
-        //checking if target exists in the given arr
-        if(nums[0] > target || nums[nums.length - 1] < target) return new int[]{-1,-1};
-        
-        int first = binarySearchFirst(nums,target);
-        if(first == -1) return new int[] {-1,-1};    // condition: if first index of target doesnt exist no need to find last
-        
-        int last = binarySearchLast(nums, target);
-        return new int[]{first,last};
+        int[] result = new int[2];
+        result[0] = findLeftIndex(nums, target);
+        result[1] = findRightIndex(nums, target);
+        return result;
+    
     }
     
-    
-    private int binarySearchFirst(int[] nums, int target){
-        int low = 0;
-        int high = nums.length - 1;
+    public int findLeftIndex(int[] nums, int target){
+        int index = -1;
+        int start = 0;
+        int end = nums.length - 1;
         
-        while(low <= high){
-            int mid = low + (high-low)/2;
+        while(start <= end){ 
+            int mid = start + (end - start)/2;
+            
             if(nums[mid] == target) {
-                //if the mid is actually very first element of the target
-                if( mid == 0 ||nums[mid - 1] != nums[mid]) return mid;
-                else {
-                    //keep moving left
-                    
-                    high = mid - 1;
-                }
-
-            } else if(nums[mid] > target){
-                high = mid - 1;
-            } else{
-                low = mid + 1;
+                index = mid;
             }
-        } 
-        return -1;
-        
+            
+            if(nums[mid] >= target ){
+                end = mid - 1;
+            } else{
+                start = mid + 1;
+            }
+        }
+        return index;
     }
     
-    
-     private int binarySearchLast(int[] nums, int target){
-        
-        int low = 0;
-        int high = nums.length - 1;
-        
-        while(low <= high){
-            int mid = low + (high-low)/2;
-            if(nums[mid] == target) {
-                //if the mid is actually very last element of the target
-                if( mid == nums.length-1 ||nums[mid + 1] != nums[mid]) return mid;
-                else {
-                    //keep moving right
-                    
-                    low = mid + 1;
-                }
-
-            } else if(nums[mid] > target){
-                high = mid - 1;
-            } else{
-                low = mid + 1;
+     public int findRightIndex(int[] nums, int target){
+        int index = -1;
+        int start = 0;
+        int end = nums.length - 1;
+            
+            while(start <= end){
+                int mid = start + (end - start)/2;
+                 if(nums[mid] == target) {
+                index = mid;
             }
-        } 
-        return -1;
-        
+                
+                if(nums[mid] <= target ){
+                    start = mid + 1;
+                } else{
+                    end = mid - 1;
+                }
+            }
+            
+            
+        return index;
     }
+    
 }
--- findMinInRotatedSortedArr.java
+++ findMinInRotatedSortedArr.java
@@ -1,34 +1,27 @@
+//TC: O(log n)
+//SC: O(1)
 
 class Solution {
     public int findMin(int[] nums) {
-        
-        //null check
-        if(nums == null || nums.length == 0) return -1;
-        //returns only element present in given arr
+
+        //null checks
+        if(nums.length == 0) return -1;
         if(nums.length == 1) return nums[0];
         
+        int start = 0;
+        int end = nums.length - 1;
         
-        int low = 0;
-        int high = nums.length -1;
-        
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            
-            //if mid is the minimum
-            
-            if((mid == 0 || nums[mid] < nums[mid - 1]) && (mid == nums.length - 1 || nums[mid] < nums[mid + 1])){
-                 return nums[mid];
-            } else if (nums[mid] < nums[high]){ //if right is sorted
-                high = mid - 1;
-                
-            }else{
-                low = mid + 1;
+        while(start < end){
+            int mid = start + (end - start)/2;
+            if(mid > 0 && nums[mid] < nums[mid-1]){
+                return nums[mid] ;
+            } else if(nums[mid] >= nums[start] && nums[mid] > nums[end]) {
+                 start = mid + 1;
+            } else {
+                end = mid - 1;
             }
-               
-              
+
         }
-    
-        return 7272;
-    
+        return nums[start] ;
     }
 }
