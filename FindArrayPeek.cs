15060d7e81a427c430c0258d156ce83099e22119
Rincy Soman <rincysoman@gmail.com>
2022-04-29 17:38:09 -0700

    Binary Search - 2 - Find Peek of Array & Find First and Last position in sorted array

---
+++ FindArrayPeek.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Algorithms
+{
+    public class FindArrayPeek
+    {
+        /// Time Complexity : O(log(n))
+        // Space Complexity :O(1)
+        // Did this code successfully run on Leetcode :Yes
+        // Any problem you faced while coding this :  Refered Online
+
+        public int FindPeakElement(int[] nums)
+        {
+            int low = 0, high = nums.Length - 1;
+            int mid;
+            while (low < high)
+            {
+                mid = low + (high - low) / 2;
+                if (nums[mid] > nums[mid + 1])
+                {
+                    high = mid;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return low;
+
+        }
+    }
+}
+}
---
+++ FirstAndLastPositionInSortedArray.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Algorithms
+{
+    public class FirstAndLastPositionInSortedArray
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+            int firstOccurance = findBound(nums, target, true);
+            if (firstOccurance == -1)
+            {
+                return new int[] { -1, -1 };
+            }
+            int lastOccurance = findBound(nums, target, false);
+            return new int[] { firstOccurance, lastOccurance };
+        }
+
+        private int findBound(int[] nums, int target, bool isFirst)
+        {
+            int low = 0, high = nums.Length - 1;
+            int mid;
+            while (low <= high)
+            {
+                mid = low + (high - low) / 2;
+                if (nums[mid] == target)
+                {
+                    if (isFirst)
+                    {
+                        if (mid == low || nums[mid - 1] != target)
+                        {
+                            return mid;
+                        }
+                        high = mid - 1;
+                    }
+                    else
+                    {
+                        if (mid == high || nums[mid + 1] != target)
+                        {
+                            return mid;
+                        }
+                        low = mid + 1;
+                    }
+                }
+                else if (nums[mid] > target)
+                {
+                    high = mid - 1;
+                }
+                else
+                {
+                    low = mid + 1;
+                }
+            }
+            return -1;
+        }
+
+
+    }
+}
