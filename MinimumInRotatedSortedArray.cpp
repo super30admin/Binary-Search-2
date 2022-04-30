cc90c7731bce39eed58f7fc0895efbf7fee68e39
Hemanth Teja <42966988+HemanthTejaY@users.noreply.github.com>
2022-04-29 21:46:43 -0400

    Update MinimumInRotatedSortedArray.cpp

--- MinimumInRotatedSortedArray.cpp
+++ MinimumInRotatedSortedArray.cpp
@@ -1,3 +1,12 @@
+// Time Complexity : O(logN)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : Yes , took lots of time
+
+// Your code here along with comments explaining your approach
+//In a rotated sorted array, one side is always sorted at any given point. So the idea is to go to the unsorted side to find the minimum 
+//We keep doing this, until our low pointer reaches the min.
+
 class Solution {
 public:
     int findMin(vector<int>& nums) 
