92f11a364c9e7a393a5a6e887b9b2a72a64b3053
aashutoshtaikar <aashutoshtaikar@gmail.com>
2022-05-03 17:48:18 -0400

    add log n solution approach

--- Problem1.java
+++ Problem1.java
@@ -2,10 +2,15 @@ import java.util.Arrays;
 
 // s30 Problem #8 - medium
 // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
-// Time Complexity : O(log n)
+// Time Complexity : O(n) 
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
+/**
+ * Problem 1 Approach 1
+ * 1. binary search to find the target element  - O(log n)
+ * 2. two pointer to scan for the first and last index of the target element - O(n)
+ */
 public class Problem1{
     public int[] searchRange(int[] nums, int target) {
         int low = 0;
@@ -58,4 +63,84 @@ public class Problem1{
         int[] result = new Problem1().searchRange(nums, target);
         Arrays.stream(result).forEach(e->System.out.print(e + " "));
     }
+}
+
+/**
+ * Problem1 Approach2
+ * using only Binary Search
+ * Time complexity: O(log n)
+ * Space complexity: O(1);
+ * 
+ * 1. locate the element using binarySearchFirst mid pointer
+ * 2. if element is located
+ *        if mid-1 is smaller than mid then return mid (first index)
+ *        else keep moving to the left
+ * 3. use the first index as low for the binarySearchSecond and locate the element
+ * 4. if the element is located 
+ *        if mid+1 is greater than mid then return mid (second index)
+ *        else keep moving to the right
+ *        
+ */
+class P1Approach2{
+
+    public int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+        
+        if (nums == null || n == 0) return new int[]{-1, -1};
+        if(nums[0] > target || nums[n - 1] < target) return new int[]{-1, -1};
+        
+        int first = binarySearchFirst(nums, target);
+        if(first == -1) return new int[]{-1, -1};
+        
+        int second = binarySearchSecond(first, nums, target);
+        return new int[]{first, second};
+        
+    }
+    
+    public int binarySearchFirst(int[] nums, int target){
+        int low = 0;
+        int high = nums.length - 1;
+        
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            
+            if(nums[mid] == target){
+                if(mid==0 || nums[mid-1] < nums[mid]){
+                    return mid;
+                } else {
+                    high = mid - 1;
+                }
+                
+            } else if (nums[mid] < target){
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+    
+    public int binarySearchSecond(int first, int[] nums, int target){
+        int low = first;
+        int high = nums.length - 1;
+        
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            
+            if(nums[mid]==target){
+                if(mid==nums.length-1 || nums[mid+1] > nums[mid]){
+                    return mid;
+                } else {
+                    low = mid + 1;
+                }
+                    
+            } else if (nums[mid] < target){
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return 88888;
+    }
+
 }
