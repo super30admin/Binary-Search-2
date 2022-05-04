0611ff1ea42b864d401e45e3d5d7aa287573dc27
aashutoshtaikar <aashutoshtaikar@gmail.com>
2022-05-04 14:18:09 -0400

    added similar approaches

--- Problem2.java
+++ Problem2.java
@@ -43,9 +43,7 @@ public class Problem2 {
         int high = nums.length - 1;  
         
         // if sorted array is not rotated at all 
-        if (nums[0] < nums[high]) {
-            return nums[0];
-        }
+        if (nums[0] < nums[high]) return nums[0];
         
         while(low <= high){
             int mid = low + (high-low)/2;
@@ -78,5 +76,56 @@ public class Problem2 {
         // {1}
         int result = new Problem2().findMin(nums);
         System.out.println(result); 
+    }
+}
+
+/**
+ * Only check the next element 
+ */
+class Problem2Approach2 {
+    public int findMin(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        int low = 0;
+        int high = nums.length - 1;
+
+        while(low <= high){
+            if(nums[low] <= nums[high]) return nums[low];
+            int mid = low + (high-low)/2;
+            
+            if(nums[mid] > nums[mid+1]) 
+                return nums[mid+1];
+            else if(nums[low] <= nums[mid]){ // if sorted part move to unsorted
+                low = mid + 1;
+            } else {
+                high = mid; // cannot skip past the smaller nums[mid] as we need to find min
+            }
+        }
+        return 8888;
+    }
+}
+
+/**
+ * Check both prev and next element of the mid
+ */
+class Problem2Approach3 {
+    public int findMin(int[] nums) {
+        if(nums.length == 1) return nums[0];
+        int low = 0;
+        int high = nums.length - 1;
+
+        while(low <= high){
+            if(nums[low] <= nums[high]) return nums[low];
+            int mid = low + (high-low)/2;
+            
+            if( (mid==0 || nums[mid-1] > nums[mid]) &&
+                (mid==nums.length-1 || nums[mid] < nums[mid+1])) // mid==nums.length-1 check not required as we check 
+                return nums[mid];
+            else if(nums[low] <= nums[mid]){ // if sorted part move to unsorted
+                low = mid + 1;
+            } else {
+                high = mid - 1; // cannot skip past the smaller nums[mid] as we need to find min
+            }
+        }
+        return 8888;
     }
 }
