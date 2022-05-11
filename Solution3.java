0c163f197273285e713b08aa05abcad10399b6af
Leelamanoj-06 <68564835+Leelamanoj-06@users.noreply.github.com>
2022-05-10 22:24:37 -0400

    Completed Binary Search 2

---
+++ Solution3.java
@@ -0,0 +1,19 @@
+class Solution3 {
+    public int findPeakElement(int[] nums) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            // base case
+            if ((mid == 0 || nums[mid - 1] < nums[mid]) && (mid == n - 1 || nums[mid + 1] < nums[mid])) {
+                return mid;
+            } else if (mid > 0 && nums[mid] < nums[mid - 1]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
