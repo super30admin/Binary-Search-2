aaf65d0d4d2ecf09dfb09de5856fdffca2e9637f
akash0218 <92997084+akash0218@users.noreply.github.com>
2022-05-28 00:39:13 -0500

    finished

--- problem_1.py
+++ problem_1.py
@@ -1,34 +1,84 @@
+# Time Complexity : O(log*n) + O(log*n)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+#
+#
+# Your code here along with comments explaining your approach
+# find left-index using a binary search
+# find right-index using a binary search
+
+
 class Solution:
+    def lindex(self, nums, start, end, target):
+        while start <= end:
+            mid = start + (end - start)//2
+            if nums[mid] == target:
+                if mid == 0 or nums[mid] != nums[mid - 1]:
+                    return mid
+                end = mid - 1
+            elif nums[mid] > target:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return -1
+
+    def rindex(self, nums, start, end, target):
+        while start <= end:
+            mid = start + (end - start)//2
+            if nums[mid] == target:
+                if mid == len(nums) - 1 or nums[mid] != nums[mid + 1]:
+                    return mid
+                start = mid + 1
+            elif nums[mid] > target:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return -1
+
     def searchRange(self, nums: list[int], target: int) -> list[int]:
         start = 0
         end = len(nums) - 1
-        result = [-1, -1]
-        while start <= end:
-            mid = start + (end - start) // 2
-            if nums[mid] == target:
-                end = mid - 1
-                result[0] = mid
-            elif target > nums[mid]:
-                start = mid + 1
-            else:
-                end = mid - 1
-
-        start = 0
-        end = len(nums) - 1
-        while start <= end:
-            mid = start + (end - start) // 2
-            if nums[mid] == target:
-                start = mid + 1
-                result[1] = mid
-            elif target > nums[mid]:
-                start = mid + 1
-            else:
-                end = mid - 1
-        return result
+        left = self.lindex(nums, start, end, target)
+        right = self.rindex(nums, left, end, target)
+        return [left, right]
 
 
 check = Solution()
-print(check.searchRange([5, 7, 7, 8, 8, 10], 8))
+print(check.searchRange([5, 7, 7, 8, 8, 10], 11))
+
+
+# class Solution:
+#     def searchRange(self, nums: list[int], target: int) -> list[int]:
+#         start = 0
+#         end = len(nums) - 1
+#         result = [-1, -1]
+#         while start <= end:
+#             mid = start + (end - start) // 2
+#             if nums[mid] == target:
+#                 end = mid - 1
+#                 result[0] = mid
+#             elif target > nums[mid]:
+#                 start = mid + 1
+#             else:
+#                 end = mid - 1
+#
+#         start = 0
+#         end = len(nums) - 1
+#         while start <= end:
+#             mid = start + (end - start) // 2
+#             if nums[mid] == target:
+#                 start = mid + 1
+#                 result[1] = mid
+#             elif target > nums[mid]:
+#                 start = mid + 1
+#             else:
+#                 end = mid - 1
+#         return result
+#
+#
+# check = Solution()
+# print(check.searchRange([5, 7, 7, 8, 8, 10], 8))
 
 # def searchRange(self, nums: List[int], target: int) -> List[int]:
 #     start = 0
--- problem_3.py
+++ problem_3.py
@@ -1,26 +1,52 @@
+# Time Complexity : O(log*n)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+#
+# search towards greater element of mid
+# there will always be a peak towards the greater element than mid.
+
 class Solution:
-    def peak(self, nums, start, end):
+    def findPeakElement(self, nums: list[int]) -> int:
+        start = 0
+        end = len(nums) - 1
         while start <= end:
-            mid = start + (end - start)//2
-            if mid != 0 and mid != len(nums)-1 and nums[mid] > nums[mid+1] and nums[mid] > nums[mid-1]:
+            mid = start + (end - start) // 2
+            if (mid == 0 or nums[mid] > nums[mid - 1]) and (mid == len(nums)-1 or nums[mid] > nums[mid + 1]):
                 return mid
-            elif mid != len(nums)-1 and nums[mid] < nums[mid + 1]:
+            elif nums[mid] < nums[mid + 1]:
                 start = mid + 1
             else:
                 end = mid - 1
 
-    def findPeakElement(self, nums: list[int]) -> int:
-        start = 0
-        end = len(nums) - 1
-        mid = start + (end-start)//2
-        if mid != 0 and mid != len(nums)-1 and nums[mid] > nums[mid+1] and nums[mid] > nums[mid-1]:
-            return mid
-        if self.peak(nums, mid + 1, end):
-            return self.peak(nums, mid + 1, end)
-        if self.peak(nums, start, mid-1):
-            return self.peak(nums, start, mid-1)
-        return nums.index(max(nums[0], nums[-1]))
-
 
 check = Solution()
-print(check.findPeakElement([1, 6, 5, 4, 3]))
+print(check.findPeakElement([2, 1]))
+
+
+# class Solution:
+#     def peak(self, nums, start, end):
+#         while start <= end:
+#             mid = start + (end - start)//2
+#             if mid != 0 and mid != len(nums)-1 and nums[mid] > nums[mid+1] and nums[mid] > nums[mid-1]:
+#                 return mid
+#             elif mid != len(nums)-1 and nums[mid] < nums[mid + 1]:
+#                 start = mid + 1
+#             else:
+#                 end = mid - 1
+#
+#     def findPeakElement(self, nums: list[int]) -> int:
+#         start = 0
+#         end = len(nums) - 1
+#         mid = start + (end-start)//2
+#         if mid != 0 and mid != len(nums)-1 and nums[mid] > nums[mid+1] and nums[mid] > nums[mid-1]:
+#             return mid
+#         if self.peak(nums, mid + 1, end):
+#             return self.peak(nums, mid + 1, end)
+#         if self.peak(nums, start, mid-1):
+#             return self.peak(nums, start, mid-1)
+#         return nums.index(max(nums[0], nums[-1]))
+#
+#
+# check = Solution()
+# print(check.findPeakElement([1, 6, 5, 4, 3]))
