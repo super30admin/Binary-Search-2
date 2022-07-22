c6d42180c71e56a2bee0cdadd0e7ae846650b7b4
Jay Tanna <31876791+jaytanna61@users.noreply.github.com>
2022-07-21 22:00:39 -0400

    Create problem2.java

---
+++ problem2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int start=0,end=nums.length-1;
+       while (start < end) {
+            if (nums[start] < nums[end]) {
+                return nums[start];
+            }
+
+            int mid = start + (end - start) / 2;
+            if (nums[mid] <= nums[end]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return nums[start];
+    }
+}
