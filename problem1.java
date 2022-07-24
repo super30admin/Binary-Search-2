84ffddb0d998e631b2be2168afb34f87eac1e4cb
Jay Tanna <31876791+jaytanna61@users.noreply.github.com>
2022-07-24 14:01:35 -0400

    Update problem1.java

--- problem1.java
+++ problem1.java
@@ -1,1 +1,9 @@
 
+// Time Complexity : O(log(n))
+// Space Complexity : O(n)
+// Did this code successfully run on Leetcode : yes
+// Any problem you faced while coding this : no
+
+
+// first we check if whole array is sorted return 1st element, if not calcule the middle element. if right side of array is sorted make end equal to end 
+// else make start equal to mid +1 because since its rotated array, min value should be part on no sorted half of array.
