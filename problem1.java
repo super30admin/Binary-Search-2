579fc286b866fd76fe36ab58b5b26398f55ca524
Jay Tanna <31876791+jaytanna61@users.noreply.github.com>
2022-07-24 14:27:55 -0400

    Update problem1.java

--- problem1.java
+++ problem1.java
@@ -5,5 +5,50 @@
 // Any problem you faced while coding this : no
 
 
-// first we check if whole array is sorted return 1st element, if not calcule the middle element. if right side of array is sorted make end equal to end 
-// else make start equal to mid +1 because since its rotated array, min value should be part on no sorted half of array.
+// we can use 2 binary search here. first to find the starting index of given target value and 2nd to find the ending index of the target value. To 
+// optimize it, we can use the starting index from the first binary search for the 2nd binary search.
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int first = binarySearchFirst(nums,0,nums.length-1,target);
+        if(first == -1)
+            return new int[] {-1,-1};
+        int last = binarySearchLast(nums,first,nums.length-1,target);
+        
+        return new int[] {first,last};
+    }
+    
+    public int binarySearchFirst(int[] nums,int start,int end,int target){
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            
+            if(nums[mid]==target){
+                if(mid == 0 || nums[mid-1] < target)
+                    return mid;
+                else
+                    end = mid-1;
+             }
+            else if(nums[mid]<target)
+                start = mid+1;
+            else
+                end = mid-1;
+        }
+        return -1;
+    }
+    
+    public int binarySearchLast(int[] nums,int start,int end,int target){
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid]==target){
+                if(mid == nums.length-1 || nums[mid+1] > target)
+                    return mid;
+                else
+                    start = mid+1;
+             }
+           else if(nums[mid]<target)
+                start = mid+1;
+            else
+                end = mid-1;
+        }
+        return -1;
+    }
+}
