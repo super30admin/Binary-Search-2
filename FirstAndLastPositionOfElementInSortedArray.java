63f524e1ecc3e2036d05865e774319f1608f3b4d
NeelakantaBhagavatula <neelakanta.bhagavatula@gmail.com>
2022-07-25 14:52:11 -0700

    First and Last index of Sorted Array

--- FirstAndLastPositionOfElementInSortedArray.java
+++ FirstAndLastPositionOfElementInSortedArray.java
@@ -6,33 +6,63 @@
 // Your code here along with comments explaining your approach
 
 class Solution {
-	public boolean searchMatrix(int[][] matrix, int target) {
-		int row = matrix.length, column = matrix[0].length - 1;
-
-		for (int i = 0; i < row; i++) {
-			// traverse through each row and apply binary search on each row if the target
-			// less than or equal to the last elemnt of each row
-			if (matrix[i][column] >= target) {
-				return binarySearch(matrix[i], target, 0, column);
-			}
-		}
-
-		return false;
-	}
-
-	private boolean binarySearch(int[] bar, int target, int start, int end) {
-
-		while (start <= end) {
-			int mid = start + (end - start) / 2;
-
-			if (bar[mid] == target)
-				return true;
-			else if (bar[mid] < target)
-				start = mid + 1;
-			else
-				end = mid - 1;
-		}
-
-		return false;
-	}
+    public int[] searchRange(int[] nums, int target) {
+        if (nums == null || nums.length == 0) return new int[] {-1, -1};
+        
+        int length = nums.length-1;
+        
+        if (nums[0] > target || nums[length] < target) return new int[] {-1, -1};
+        
+        int firstIndex = getFirstIndex(nums, target);
+        
+        if (firstIndex != -1) {
+            int lastIndex = getLastIndex(nums, target, firstIndex, length);
+            
+            return new int[] {firstIndex, lastIndex};
+        }
+        
+        return new int[] {-1, -1};
+    }
+    
+    private int getFirstIndex(int[] nums, int target) {
+        int start = 0, end = nums.length-1;
+        
+        while (start <= end) {
+            int mid = start + (end-start)/2;
+            
+            if (nums[mid] == target) {
+                if (mid == 0 || nums[mid] > nums[mid-1]) {
+                    return mid;
+                } else { // check in left half of array
+                    end = mid - 1;
+                }
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
+    
+    private int getLastIndex(int[] nums, int target, int start, int end) {
+        while (start <= end) {
+            int mid = start + (end-start)/2;
+            
+            if (nums[mid] == target) {
+                if (mid == nums.length-1 || nums[mid] < nums[mid+1]) {
+                    return mid;
+                } else { // check in right half of array
+                    start = mid + 1;
+                }
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        return -1;
+    }
 }
