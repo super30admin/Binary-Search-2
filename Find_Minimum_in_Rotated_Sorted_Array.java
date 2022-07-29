22b0cbf6a5d20e40b6a27fb6c9446c9c26e18bdd
madhuri434 <madhuri1.ranvirkar@gmail.com>
2022-07-29 11:14:36 -0700

    binary-search-2

---
+++ Find_Minimum_in_Rotated_Sorted_Array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int findMin(int[] nums) {
+        if(nums[0] <= nums[nums.length - 1]) return nums[0];
+        if(nums.length == 1) return nums[0];
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high){
+            if(nums[low] <= nums[high]) return nums[low];
+            int mid = low + (high - low)/2;
+
+            if((mid == 0 || nums[mid] < nums[mid - 1]) &&(mid == nums.length -1 || nums[mid] < nums[mid +1])){
+
+                return nums[mid];
+            }
+            // which part is sorted
+            else if(nums[low] <= nums[mid]){
+                low = mid + 1;
+            }
+            else{
+                high  = mid - 1;
+            }
+        }
+
+        return 9549458;
+    }
+}
