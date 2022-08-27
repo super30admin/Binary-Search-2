1c49b9f8f50f91d4bf120544dfff6253339502d3
Rajendra Prasad Patil <111140820+rajendra7406-2@users.noreply.github.com>
2022-08-27 19:05:44 -0400

    Solved

--- problem3.py
+++ problem3.py
@@ -1,6 +1,30 @@
-# I couldn't solve this because I didnot solve find min element using binary search
+'''
+162. Find Peak Element
+TIME COMPLEXITY: O(logN) because it is a binary search
+SPACE COMPLEXITY: O(1)
+LEETCODE: Yes
+DIFFICULTIES: Nope
+'''
 
+# Binary search is either to move left part or right part
+# direction is decided, based up on where, highest element than middle element lies
 
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        l = 0
+        h = n-1
+        
+        while l<=h:
+            m = (l+h)//2
+            if l == m:
+                if nums[l] < nums[h]:
+                    return h
+                else:
+                    return l
+            if nums[m] < nums[m+1] and m+1 <= h:
+                l = m + 1
+            else:
+                h = m
+                
         
