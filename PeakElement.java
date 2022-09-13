bcfa5323037c2ba68d44a844d057b4116684e6d3
Sumanth Shankar <sumanth@rezy360.com>
2022-09-13 12:41:09 -0700

    Completed find peek element

---
+++ PeakElement.java
@@ -0,0 +1,27 @@
+class PeakElement {
+    public int findPeakElement(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return -1;
+        }
+
+        int n = nums.length;
+        int low = 0; 
+        int high = n - 1;
+
+		while(low <= high) {
+			int mid = (low + (high - low)) / 2;
+
+			if((mid == 0 || nums[mid - 1] <= nums[mid]) &&
+				(mid == n - 1 || nums[mid + 1] <= nums[mid])) {
+                    return mid;
+                }
+			if(mid > 0 && nums[mid - 1] >= nums[mid]) {
+                high = mid -1;
+            }
+			else {
+                low = mid + 1;
+            }
+		}
+		return -1;
+    }
+}
