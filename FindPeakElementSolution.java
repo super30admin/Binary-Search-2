57e68075b2bb8566672307da11c3d4d5cab8981b
abhinavvkashyap <99699489+abhinavvkashyap@users.noreply.github.com>
2022-10-10 11:34:57 -0700

    Create FindPeakElementSolution.java

---
+++ FindPeakElementSolution.java
@@ -0,0 +1,18 @@
+class FindPeakElementSolution 
+{
+public int findPeakElement(int[] nums)
+{
+int start =0, end= nums.length-1;
+while(end>=start)
+{
+int mid = (start+end)/2;
+if((mid==0||nums[mid-1]<=nums[mid])&&(mid==(nums.length-1)||nums[mid]>nums[mid+1])) 
+    return mid;
+if(mid>0&&nums[mid]>=nums[mid+1])
+    end = mid-1;
+else
+    start = mid+1;
+}
+return -1;
+}
+}
