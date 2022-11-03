dadf35ead25d4c4c74924edd64bfbce1f2be8a9d
katta332 <praveenkumar.kpc@gmail.com>
2022-11-02 22:37:31 -0600

    add approach learn from class

--- FindPositions.java
+++ FindPositions.java
@@ -3,34 +3,50 @@
 //able to submit code in leetcode
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int i = helper(nums, target);
-        if(i==-1)
-        {
-            return new int[]{-1, -1};
-        }
-        int j = i+1;
-        while(i>=0 && nums[i]==target){
-            i = i-1;
-        }
-        while(j<nums.length && nums[j]==target){
-            j = j+1;
-        }
-        return new int[]{i+1, j-1};
+        
+        return new int[]{findleft(nums, target), findright(nums, target)};
     }
-    public int helper(int[] nums, int target){
-        int l=0;
+    public int findleft(int[] nums, int target){
+        int l = 0;
         int r = nums.length-1;
         while(l<=r){
+            //System.out.println(l);
+            //System.out.println(r);
             int m = l+(r-l)/2;
             if(nums[m]==target){
-                return m;
+                if((m)==0 || nums[m-1]!=target){
+                    return m;
+                }
+                r=m-1;
             }
-            else if(nums[m]<target){
+            else if(target>nums[m]){
                 l=m+1;
             }
-            else
-            {
-                r= m-1;
+            else{
+                r = m-1;
+            }
+        }
+        return -1;
+    }
+
+    public int findright(int[] nums, int target){
+        int l = 0;
+        int r = nums.length-1;
+        while(l<=r){
+            //System.out.println(l);
+            //System.out.println(r);
+            int m = l+(r-l)/2;
+            if(nums[m]==target){
+                if((m)==r || nums[m+1]!=target){
+                    return m;
+                }
+                l=m+1;
+            }
+            else if(nums[m]>target){
+                r = m-1;
+            }
+            else{
+                l=m+1;
             }
         }
         return -1;
