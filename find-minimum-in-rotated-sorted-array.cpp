91e62d72cb7072521182c6997d2b1272e68519cc
Rahul Raj <rahulraj@Rahuls-Air.lan>
2022-11-03 22:24:10 -0400

    Completed Binary Search 2, Please review

--- find-minimum-in-rotated-sorted-array.cpp
+++ find-minimum-in-rotated-sorted-array.cpp
@@ -8,13 +8,15 @@ class Solution {
 public:
     int findMin(vector<int>& nums) {
         int l=0, r=nums.size()-1;
-        int minElement = INT_MAX;
         while(l<=r) {
+            if(nums[l] <= nums[r]) return nums[l];   //edge case when array is sorted on current logic, also an optimization
+            
             int mid = l+(r-l)/2;
-            minElement = min(minElement, nums[mid]);
-            if(nums[mid] < nums[r]) r=mid-1;   //sorted on the right side
-            else   minElement = min(minElement, nums[l]), l=mid+1;  //sorted on the left side
+            if((mid == l || nums[mid] < nums[mid-1]) 
+                && (mid == r || nums[mid] < nums[mid+1]))    return nums[mid];   //to instantly find minimum
+            else if(nums[mid] <= nums[r]) r=mid-1;   //sorted on the right side, so go left to find pivot
+            else   l=mid+1;  //sorted on the left side, so go right to find the pivot
         }
-        return minElement;
+        return -1;
     }
 };
