e6536359eec1e246af28602b8f516cd5f7a02188
naveenpi <gsnb.griet@gmail.com>
2023-01-02 09:43:21 -0600

    binary search -2 assignment

---
+++ Problem3.java
@@ -0,0 +1,27 @@
+public class Problem3 {
+
+    //o(log(n))
+    public int findPeakElement(int[] nums) {
+        int n =  nums.length;
+        int low = 0;
+        int high = nums.length - 1;
+
+        while(low <= high){
+
+            int mid = low + (high - low)/2 ;
+
+            if((mid == 0 || nums[mid] > nums[mid-1])
+                    && (mid == n-1 || nums[mid] > nums[mid+1])){
+                return mid;
+            }
+            else if(mid > 0 && nums[mid] < nums[mid-1]){
+                high = mid-1;
+            }
+            else{
+                low = mid + 1;
+            }
+
+        }
+        return 0;
+    }
+}
