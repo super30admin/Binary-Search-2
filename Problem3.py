55f71086734b8e89564d20c0330d28e3f930741f
AnirudhS30 <127216139+AnirudhS30@users.noreply.github.com>
2023-03-21 19:11:36 -0400

    Create Problem3.py

---
+++ Problem3.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        if nums is None or len(nums)==0:
+            return int(sys.maxsize)
+        low= 0
+        high= len(nums)-1
+
+        while low<=high:
+            mid= low + (high-low)//2
+
+            # If mid is peek element.
+            if (mid== low or nums[mid-1] < nums[mid]) and (mid== high or nums[mid+1] < nums[mid]):
+                return mid
+            elif nums[mid]<nums[mid+1]:
+                low= mid+1
+            else:
+                high= mid-1
