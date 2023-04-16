f995cb62dddeb054e7f70429d3725b9849c1b083
Apurva Kemkar <apurvakemkar@Apurvas-MBP.lan>
2023-04-16 15:11:10 -0500

    Binary Search-2 completed. FindMin, FindPeak and seachFirstLastRange. Please review. Thank you.

---
+++ FindPeakElement.java
@@ -0,0 +1,17 @@
+// Time Complexity : O(logN)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this :
+
+
+// Your code here along with comments explaining your approach in three sentences only
+/*
+ * We observed that in rotated array, atleast one side of the array is sorted but the minimun element lies in the unsorted part.
+ * We can say minimum element is lesser that the left and right element of that element and we return.
+ * else we go in left or right side depennfing on the unsorted part.
+ */
+public class FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        
+    }
+}
