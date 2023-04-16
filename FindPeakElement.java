e2375c169927b584df9937fe43d2844f1be7a7b5
Apurva Kemkar <apurvakemkar@Apurvas-MBP.lan>
2023-04-16 15:14:37 -0500

    Binary Search-2 completed. FindMin, FindPeak and seachFirstLastRange. Please review. Thank you.

--- FindPeakElement.java
+++ FindPeakElement.java
@@ -6,12 +6,27 @@
 
 // Your code here along with comments explaining your approach in three sentences only
 /*
- * We observed that in rotated array, atleast one side of the array is sorted but the minimun element lies in the unsorted part.
- * We can say minimum element is lesser that the left and right element of that element and we return.
- * else we go in left or right side depennfing on the unsorted part.
+ * As we need to right algorithm in O(logn) We try to implement binary search  where we check if mid element is greater than both the side,
+ * then we return mid. Else we know mid element is lesser than one on right or left element and we traverse to the side which has higher values
+ * or return -1 if not found.
  */
 public class FindPeakElement {
     public int findPeakElement(int[] nums) {
-        
+        if(nums == null || nums.length < 1) {
+            return -1;
+        }
+        int low = 0;
+        int high = nums.length - 1;
+        while(low <= high) {
+            int mid = low + (high - low) / 2;
+            if((mid == low || nums[mid - 1] < nums[mid]) && (mid == high || nums[mid] > nums[mid + 1])) {
+                return mid;
+            } if (nums[mid] < nums[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return -1;
     }
 }
