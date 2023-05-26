896fb86cc65e87c3f30777a00c470618d263b6ca
vyankateshmisar <venkymisar@gmail.com>
2023-05-26 18:25:45 -0400

    Completed Binary Search-2

---
+++ Problem_3.py
@@ -0,0 +1,38 @@
+"""
+Problem : 3
+
+Time Complexity : O(logn)
+Space Complexity : O(1)
+
+Did this code successfully run on Leetcode : Yes
+Any problem you faced while coding this : No
+
+
+Checking base cases when the there is just one element in the array
+Else checking the values adjacent to the middle value, if its the peak, 
+then returning the mid value, else performing binary search on the either side
+"""
+
+class Solution(object):
+    def findPeakElement(self, nums):
+        l,r=0,len(nums)-1
+
+        while l<r:
+            mid=l+(r-l)//2
+            if mid==0:
+                if nums[0]>nums[1]:
+                    return 0
+                else:
+                    return 1
+            if mid==r:
+                if nums[r-1]>=nums[r-2]:
+                    return r-1
+                else:
+                    return r-2
+            if nums[mid]>nums[mid+1] and nums[mid]>nums[mid-1]:
+                return mid
+            if nums[mid]<nums[mid-1]:
+                r=mid-1
+            else:
+                l=mid+1
+        return l
