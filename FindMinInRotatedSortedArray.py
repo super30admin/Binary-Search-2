3c3f5a8b4f3d02a64c0534ec594f41f129e7080e
Saloni Modi <salonimodi@Salonis-MBP.lan>
2023-05-27 16:54:09 -0700

    Binary Search 2

---
+++ FindMinInRotatedSortedArray.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        low = 0
+        high = len(nums)-1
+        
+        while low <= high:
+            mid = low + (high - low) // 2 
+            if nums[mid] > nums[high]:
+                    low = mid + 1
+            elif nums[mid] < nums[high]:
+                    high = mid
+            else:
+                    high -= 1
+
+        return nums[low]
