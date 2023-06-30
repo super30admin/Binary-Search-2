fd4574b8b01ea06a6d10c71b93567cb20d228681
somonnoy <sb7238@g.rit.edu>
2023-06-30 19:40:27 -0400

    Applied binary search to FirstAndLastPositionOfElement

--- FirstAndLastPositionOfElement.java
+++ FirstAndLastPositionOfElement.java
@@ -1,4 +1,4 @@
-// Time Complexity : O(n)
+// Time Complexity : O(log n)
 // Space Complexity : O(1)
 // Did this code successfully run on Leetcode : Yes
 // Any problem you faced while coding this : No
@@ -7,34 +7,55 @@
 public class FirstAndLastPositionOfElement {
 
     public int[] searchRange(int[] nums, int target) {
+        int[] result = new int[2];
+        result[0] = findFirstPosition(nums, target);
+        result[1] = findLastPosition(nums, target);
+        return result;
+    }
 
-        int result[] = new int[2];
+    private int findFirstPosition(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int position = -1;
 
-        result[0] = -1;
-        result[1] = -1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-
-        int count = 0;
-
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(nums[i] == target)
-            {
-                count++;
-
-                if(count == 1)
-                {
-                    result[0] = i;
+            // First, we intend to remove the right side of the array as we are searching for first position
+            if (nums[mid] >= target) {
+                right = mid - 1;
+                if (nums[mid] == target) {
+                    position = mid;
                 }
-
-                result[1] = i;
-
+            } else {
+                left = mid + 1;
             }
-
         }
 
-        return result;
+        return position;
+    }
 
+    private int findLastPosition(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int position = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+
+            // First, we intend to remove the left side of the array as we are searching for last position
+            if (nums[mid] <= target) {
+                left = mid + 1;
+                if (nums[mid] == target) {
+                    position = mid;
+                }
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return position;
     }
 
 }
