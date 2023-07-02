caf4cfaf0ddafd50db44331099a8284181f11188
yashatre98 <yashatre98@gmail.com>
2023-07-01 23:34:41 -0700

    binary search 2 final

--- find_min.py
+++ find_min.py
@@ -1,3 +1,15 @@
+# // Time Complexity :  O(log(N))
+# // Space Complexity : no extra space used
+# // Did this code successfully run on Leetcode : Yes
+# // Any problem you faced while coding this :  
+
+# // Your code here along with comments explaining your approach:   
+# 1) check if array is sorted, put in the while loop so the narrowers arrays also get checked.
+# 2) if we get a mid which is a valley then return that or break.
+# 3) if left side is sorted move right.
+# 4) else move left.
+
+
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         l=0
