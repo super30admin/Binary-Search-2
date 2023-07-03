e2812a6fd3f1a73c4ab382d45bb23f749df56bea
ayushichourasia02 <ayushichourasia02@gmail.com>
2023-07-03 17:17:51 -0500

    Completed - Problem 10 - Find Index of First and Last Element in Sorted Array

---
+++ FindFIrstAndLastIndexOfTarget.java
@@ -0,0 +1,58 @@
+class FindFirstAndLastIndex0fTarget {
+    public int[] searchRange(int[] arr, int target) {
+        if(arr == null || arr.length == 0) return new int[] {-1,-1};
+        int l = 0;
+        int n = arr.length-1;
+        int h = n;
+        if(arr[l] > target || arr[h] < target) return new int[] {-1,-1};
+        //left side Binary search
+        int leftIndex = SearchLeftIndex(arr,l,h,target);
+        if(leftIndex == -1) return new int[] {-1,-1};
+        int rightIndex = SearchRightIndex(arr, l, n, target);
+        return new int[] {leftIndex,rightIndex};
+    }
+
+    private int SearchLeftIndex(int[] arr,int l, int h, int target){
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            if(arr[mid] == target){
+                if(mid==l || arr[mid] > arr[mid-1]){
+                    return mid;
+                }
+                else{
+                    h=mid-1;
+                }
+            }
+            else if(arr[mid] > target){
+                h = mid-1;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        return -1;
+    }
+
+    private int SearchRightIndex(int[] arr, int l, int h, int target){
+        while(l<=h) {
+            int mid = l + (h-l)/2;
+            if(arr[mid] == target){
+                if(mid==h || arr[mid] < arr[mid+1])
+                {
+                    return mid;
+                }
+                else{
+                    l = mid+1;
+                }
+            }
+            else if (arr[mid] > target){
+                h=mid-1;
+            }
+            else{
+                l = mid+1;
+            }
+        }
+        return -1;
+
+    }
+}
