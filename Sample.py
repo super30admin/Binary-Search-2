fb5c7e02a5b9aae08f9504f5a5511f598d668e92
gunjan dayani <41314977+dgh007786@users.noreply.github.com>
2023-07-03 21:26:12 -0400

    finished

--- Sample.py
+++ Sample.py
@@ -47,4 +47,52 @@ class Solution:
             return [-1,-1]
         last = self.binarySerachLast(nums,first,n-1,target)
         return (first,last)
-        
+
+# Time complexity = O(logn)
+# Space complexity = O(1)
+# Yes
+# No
+
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        if(len(nums)==1): return 0
+        low = 0
+        high = len(nums) - 1
+        while(low<=high):
+            mid = int(low + (high-low)/2) 
+            if((mid==0 or nums[mid]>nums[mid-1]) and 
+            (mid==len(nums)-1 or nums[mid]>nums[mid+1])):
+                return mid
+            elif(mid>0 and nums[mid]<nums[mid-1]):
+                high = mid - 1
+            else:
+                low = mid + 1
+        return -1
+
+
+# Time complexity = O(logn)
+# Space complexity = O(1)
+# Yes
+# No
+
+#always lies on unsorted side
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        low = 0
+        high = len(nums) - 1
+        while(low<=high):
+            if(nums[low]<=nums[high]):
+                return nums[low]
+            mid = int(low + (high-low)/2) 
+            if((mid==0 or nums[mid]<nums[mid-1]) and 
+            (mid==len(nums)-1 or nums[mid]<nums[mid+1])):
+                return nums[mid]
+            elif(nums[low]<=nums[mid]):
+                # left is sorted
+                low = mid + 1
+            else:
+                high = mid - 1
+        return -1
+
+
