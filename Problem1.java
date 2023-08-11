f24d8692e6850d30c24ec03bf90e064727b334a7
RudranilMitra <62679743+RudranilMitra@users.noreply.github.com>
2023-08-11 07:37:40 -0700

    Update Problem1.java

--- Problem1.java
+++ Problem1.java
@@ -1,3 +1,7 @@
+// Time Complexity : O(log n)
+// Space Complexity : O(1)
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 class Solution {
     public int binarySearchRight(int[] nums,int low, int target){
