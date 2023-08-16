15258066ed1fff47a4eed501090fa80ee5a2f436
akshay <er.akshay@gmail.com>
2023-08-17 00:24:21 +0530

    update the TC SC

--- Problem_2.java
+++ Problem_2.java
@@ -1,22 +1,24 @@
+//TC: O(log n).
+//SC: O(1)
 public class Problem_2 {
     public int findMin(int[] nums) {
-        if(nums == null || nums.length == 0)    return -1;
+        if (nums == null || nums.length == 0) return -1;
         int n = nums.length;
-        int low=0, high = n-1;
-        while(low<=high){
-            if(nums[low] <= nums[high]){
+        int low = 0, high = n - 1;
+        while (low <= high) {
+            if (nums[low] <= nums[high]) {
                 //means complete array is sorted
                 return nums[low];
             }
-            int mid = low + (high-low)/2;
+            int mid = low + (high - low) / 2;
 
-            if((mid==0 || nums[mid] < nums[mid-1] ) && (mid == n-1 || nums[mid] < nums[mid+1])){
+            if ((mid == 0 || nums[mid] < nums[mid - 1]) && (mid == n - 1 || nums[mid] < nums[mid + 1])) {
                 return nums[mid];
-            } else if(nums[low] <= nums[mid]){
+            } else if (nums[low] <= nums[mid]) {
                 //left side is sorted, so pivot wont lie this side
-                low = mid+1;
+                low = mid + 1;
             } else {
-                high = mid-1;
+                high = mid - 1;
             }
         }
         return -1;
--- Problem_3.java
+++ Problem_3.java
@@ -1,3 +1,5 @@
+//TC: O(log n).
+//SC: O(1)
 public class Problem_3 {
     public int findPeakElement(int[] nums) {
         int n = nums.length;
@@ -9,27 +11,27 @@ public class Problem_3 {
         if (nums[n - 1] > nums[n - 2])
             return n - 1;
 
-        int low = 0, high = n-1;
+        int low = 0, high = n - 1;
         int ans = binarySearch(nums, low, high);
         return ans;
     }
 
-    int binarySearch(int[] nums, int low, int high){
+    int binarySearch(int[] nums, int low, int high) {
         int n = nums.length;
-        int mid = low + (high-low)/2;
+        int mid = low + (high - low) / 2;
         //check if mid is peak
         //Compare middle element with its neighbours
-        if((mid==0 || nums[mid]> nums[mid-1]) && (mid==n-1 || nums[mid]>nums[mid+1]))
+        if ((mid == 0 || nums[mid] > nums[mid - 1]) && (mid == n - 1 || nums[mid] > nums[mid + 1]))
             return mid;
 
         //if mid is not peak, peak should be on the higher slope side.
-        if(mid>0 && nums[mid-1] > nums[mid]){
+        if (mid > 0 && nums[mid - 1] > nums[mid]) {
             //left is higher side
-            return binarySearch(nums, low, mid-1);
+            return binarySearch(nums, low, mid - 1);
 
         } else {
             //right is higher side
-            return binarySearch(nums, mid+1, high);
+            return binarySearch(nums, mid + 1, high);
         }
     }
 }
