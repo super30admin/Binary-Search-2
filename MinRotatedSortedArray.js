6fc7ef3a27dfa9d64f5170ffcd3e4ca614c6b1ac
vigneshboopalraj <Vignesh@1>
2023-09-01 20:58:42 -0700

    Completed Binary Search-2

---
+++ MinRotatedSortedArray.js
@@ -0,0 +1,45 @@
+/**
+ TC: O(log n)
+ SC: O(1)
+
+ Approch:
+
+ Since we have to do it on O(log n) times we do binary search
+
+ In a rotated sorted array the condition to rememeber is that atleast one side is always sorted
+ Also the likelyhood of the min element being in the array would be in the unsorted section
+ The smallest element in the array would be smaller than the element on the left and the right
+
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+  if (!nums) return -1;
+
+  let low = 0;
+  let high = nums.length - 1;
+
+  while (low <= high) {
+    // The first time this checks that if the entire array is sorted,
+    //  from the next time onwards then it checks within the given high and low
+    //  and if it staisfies then return nums[low]
+    if (nums[low] <= nums[high]) {
+      return nums[low]
+    }
+
+    const mid = Math.floor(low + (high-low)/2);
+    if ((mid === 0 || nums[mid] < nums[mid-1]) &&
+        (mid === nums.length -1 || nums[mid] < nums[mid+1])) {
+      return nums[mid]
+    }
+
+    if (nums[low] <= nums[mid]) { // left side is sorted
+      low = mid + 1;
+    } else {
+      high = mid - 1;
+    }
+  }
+};
