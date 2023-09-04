feb311dabce35e2368c44c5b203fa4bdf7a8f7ed
Krushika Gujarati <krushika2910@gmail.com>
2023-09-04 08:17:03 -0700

    completed find peak element

---
+++ findpeak.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        if nums == None or len(nums) == 0:
+            return -1
+        l = 0
+        r = len(nums) - 1
+
+        while l < r:
+            mid = (l+r) // 2
+
+            if (nums[mid] > nums[mid + 1]):
+                r = mid
+            else:
+                l = mid + 1
+        return l
