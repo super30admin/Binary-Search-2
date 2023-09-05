7b74a6afff66c4f11526b670e3d2b9b1ef853f29
vinith bandoju <vinith007.bandoju.com>
2023-09-04 23:19:09 -0700

    done with Peak element search

---
+++ FindPeakElement.java
@@ -0,0 +1,17 @@
+public class FindPeakElement {
+    public int findPeakElement(int[] nums) {
+        if(nums.length == 0 && nums==null)
+            return -1;
+        int low = 0, high = nums.length;
+        while(low<=high){
+            int mid = low + (high - low)/2;
+            if((mid == high || nums[mid]>nums[mid+1]) && (mid == low ||nums[mid] < nums[mid-1])){
+                return nums[mid];
+            }if(nums[mid]<nums[mid+1])
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+}
