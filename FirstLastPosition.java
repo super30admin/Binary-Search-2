4544d80729fbbd56a359c2a293116942b3dd6119
sukhpreetkaur27 <sukhpreetkaur27@ymail.com>
2023-09-29 20:52:48 -0400

    Done - Day 4 - BinarySearch2
    - Find First and Last Position of Element in Sorted Array

---
+++ FirstLastPosition.java
@@ -0,0 +1,55 @@
+public class FirstLastPosition {
+
+    public FirstLastPosition() {
+
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int lb = lowerBound(nums, target);
+        int ub = upperBound(nums, target);
+
+        return new int[] { lb, ub };
+    }
+
+    private int lowerBound(int[] nums, int target) {
+        int lb = -1;
+        int n = nums.length;
+        int lo = 0;
+        int hi = n - 1;
+        int mid;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+
+            if (nums[mid] == target) {
+                lb = mid;
+                hi = mid - 1;
+            } else if (nums[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return lb;
+    }
+
+    private int upperBound(int[] nums, int target) {
+        int ub = -1;
+        int n = nums.length;
+        int lo = 0;
+        int hi = n - 1;
+        int mid;
+        while (lo <= hi) {
+            mid = lo + (hi - lo) / 2;
+
+            if (nums[mid] == target) {
+                ub = mid;
+                lo = mid + 1;
+            } else if (nums[mid] < target) {
+                lo = mid + 1;
+            } else {
+                hi = mid - 1;
+            }
+        }
+        return ub;
+    }
+}
