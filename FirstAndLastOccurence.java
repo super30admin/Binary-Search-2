42d93909b9ac419a86ec3be9f714d11a288feaab
Anish <anishsingh0106@gmail.com>
2024-01-27 13:18:09 -0500

    Binary search 2 complete

--- FirstAndLastOccurence.java
+++ FirstAndLastOccurence.java
@@ -1,66 +1,64 @@
+/* Time Complexity : O(logn)
+Space Complexity : O(1)
+Did this code successfully run on Leetcode : Yes
+Any problem you faced while coding this : No
+Your code here along with comments explaining your approach in three sentences only
+Two binary search implemented. First to find the first occurence. Then the next one from the range of
+firstOccurence till end. To check if it's the first occurence, compare with left; for last occurence compare with right
+*/
+
 class FirstAndLastOccurence {
     public int[] searchRange(int[] nums, int target) {
-        // binary search to find the element
         int low = 0;
         int high = nums.length - 1;
-        // index where target is found
-        int mid;
-        int index = -1;
+        int firstIndex = -1;
+
+        // first binary search to calculate the first occurence
 
         while (low <= high) {
-            mid = low + (high - low) / 2;
+            int mid = low + (high - low) / 2;
 
             if (nums[mid] == target) {
-                index = mid;
-                break;
-            }
-
-            if (target > nums[mid]) {
+                // first occurence only if element to left is not equal to nums[mid] or mid == 0
+                if (mid == 0 || nums[mid - 1] != nums[mid]) {
+                    firstIndex = mid;
+                    break;
+                }
+                high = mid - 1;
+            } else if (nums[mid] < target) {
                 low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
 
-        // not found
-        if (index == -1)
+        // element not found
+        if (firstIndex == -1)
             return new int[] { -1, -1 };
 
-        // initializing to the one element that was found
-        // updating as we find one on other ends
-        int leftRes = index;
-        int rightRes = index;
-
-        // finding first occurence
-        int newHigh = index - 1;
-        low = 0;
-
-        while (low <= newHigh) {
-            mid = low + (newHigh - low) / 2;
-
-            if (nums[mid] == target) {
-                newHigh = mid - 1;
-                leftRes = mid;
-            } else {
-                low = mid + 1;
-            }
-        }
-
-        // finding second occurence
-        int newLow = index + 1;
+        // second binary search from firstIndex to last element
+        low = firstIndex;
         high = nums.length - 1;
+        int lastIndex = firstIndex;
 
-        while (newLow <= high) {
-            mid = newLow + (high - newLow) / 2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
             if (nums[mid] == target) {
-                newLow = mid + 1;
-                rightRes = mid;
+                // last occurence only if element to right is not equal to nums[mid] or mid is
+                // last index
+                if (mid == nums.length - 1 || nums[mid + 1] != nums[mid]) {
+                    lastIndex = mid;
+                    break;
+                }
+                low = mid + 1;
+            } else if (nums[mid] < target) {
+                low = mid + 1;
             } else {
                 high = mid - 1;
             }
         }
 
-        return new int[] { leftRes, rightRes };
+        return new int[] { firstIndex, lastIndex };
     }
 }
