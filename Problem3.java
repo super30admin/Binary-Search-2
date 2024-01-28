00fc1dcc64035a98e247e07211f3b98030fd2f2e
iarshtejay <arshtejay@gmail.com>
2024-01-28 18:53:36 -0400

    feat: problem 3 solution

--- Problem3.java
+++ Problem3.java
@@ -1,13 +1,30 @@
 //## Problem 3: (https://leetcode.com/problems/find-peak-element/)
 
-// Time Complexity :
-// Space Complexity :
-// Did this code successfully run on Leetcode :
-// Any problem you faced while coding this :
+// Time Complexity : O(log n) 
+// Space Complexity :   Constant space
+// Did this code successfully run on Leetcode : Yes
+// Any problem you faced while coding this : No
 
 
 // Your code here along with comments explaining your approach in three sentences only
 
-public class Problem3 {
-    
+class Problem3 {
+    public int findPeakElement(int[] nums) {
+        // The peak always lies on the increasing side
+        // We can use this fact to traverse the array and cut search space in half
+        int l=0, r=nums.length-1;
+
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            // check if peak
+            if((mid==0||nums[mid]>nums[mid-1]) &&
+                (mid==(nums.length-1)||nums[mid]>nums[mid+1]))  return mid;
+
+            // Move towards the increasing side
+            if(mid!=0 && nums[mid]<nums[mid-1])    r=mid-1;
+            else    l=mid+1;
+        }
+
+        return -1;
+    }
 }
