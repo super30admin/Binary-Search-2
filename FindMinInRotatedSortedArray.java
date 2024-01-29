7f167105ac54e20f1acba99b667d22ab5ea4d2e7
Sathya <psathya94@gmail.com>
2024-01-29 09:50:15 -0800

    Completed

--- FindMinInRotatedSortedArray.java
+++ FindMinInRotatedSortedArray.java
@@ -3,18 +3,17 @@
 //Ran in Leetcode - Yes
 
 class Solution {
-    public int findMin(int[] nums) {
-        int middle = 0;
+	public static int findMin(int[] nums) {
+		int middle = 0;
 		int low = 0;
 		int high = nums.length - 1;
 		while (low <= high) {
 			middle = low + (high - low) / 2;
-            if(nums[middle] < nums[high]){
-                high = middle - 1;
-            }
-			else 
-				low = middle + 1;	
+			if (nums[middle] < nums[high]) {
+				high = middle;
+			} else
+				low = middle + 1;
 		}
 		return nums[middle];
-    }
+	}
 }
