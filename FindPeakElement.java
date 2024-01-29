08ef45b16c8e16a1d52b6530329e3368da8c381d
Sathya <psathya94@gmail.com>
2024-01-29 00:10:09 -0800

    Completed

---
+++ FindPeakElement.java
@@ -0,0 +1,25 @@
+//Time Complexity - O(log n)
+//Space Complexity - O(1)
+//Ran in Leetcode - Yes
+
+public class FindPeakElement {
+	 public int findPeakElement(int[] nums) {
+	       int middle = 0;
+			int low = 0;
+			int high = nums.length - 1;
+			while (low <= high) {
+				middle = low + (high - low) / 2;
+				if (middle == 0 || nums[middle] > nums[middle - 1] && middle == nums.length - 1
+						|| nums[middle] > nums[middle + 1])
+					return middle;
+				else {
+					if (nums[middle + 1] > nums[middle - 1])
+						low = middle;
+					else
+						high = middle - 1;
+
+				}
+			}
+			return middle; 
+	    }	
+}
